6f7b3faf8a2b3a2847364d5439e9e4358f4b27ca
tanjinyi <tanjinyi96@gmail.com>
2019-01-15 14:03:45 +0800

    Abstract contact out from HomeComponent

---
+++ app/src/components/Contact.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="section section-pad section-bg" id="contact">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <div class="section-head">
+              <h2 class="section-title">Contact Us
+                <span>Contact</span>
+              </h2>
+              <p>Still here? Reach out to us!</p>
+            </div>
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col-lg-8 offset-lg-2">
+            <ul class="contact-info">
+              <li><em class="fab fa-linkedin" style="margin-bottom: 15px;"></em><span><a href="https://www.linkedin.com/company/toruslabs/about/">toruslabs</a></span></li>
+              <li><em class="fa fa-envelope"></em><span><a href="mailto:hello@tor.us">hello@tor.us</a></span></li>
+              <li><em class="fa fa-paper-plane"></em><span><a href="https://t.me/torusprotocol">@torusprotocol</a></span></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+<style>
+.contact-info {
+    display: flex;
+    justify-content: center;
+}
+</style>
--- app/src/components/HomeComponent.vue
+++ app/src/components/HomeComponent.vue
@@ -147,29 +147,7 @@
     <!-- Start Section -->
 
     <!-- Start Section -->
-    <div class="section section-pad section-bg" id="contact">
-      <div class="container">
-        <div class="row text-center">
-          <div class="col">
-            <div class="section-head">
-              <h2 class="section-title">Contact Us
-                <span>Contact</span>
-              </h2>
-              <p>Still here? Reach out to us!</p>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-lg-8 offset-lg-2">
-            <ul class="contact-info">
-              <li><em class="fab fa-linkedin" style="margin-bottom: 15px;"></em><span><a href="https://www.linkedin.com/company/toruslabs/about/">toruslabs</a></span></li>
-              <li><em class="fa fa-envelope"></em><span><a href="mailto:hello@tor.us">hello@tor.us</a></span></li>
-              <li><em class="fa fa-paper-plane"></em><span><a href="https://t.me/torusprotocol">@torusprotocol</a></span></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
+    <contact />
     <!-- End Section -->
 
     <!-- Start Section -->
@@ -196,7 +174,8 @@ export default {
   components: {
     Preloader: require('./Preloader.vue').default,
     WelcomeImage: require('./WelcomeImage.vue').default,
-    WelcomeBanner: require('./WelcomeBanner.vue').default
+    WelcomeBanner: require('./WelcomeBanner.vue').default,
+    Contact: require('./Contact.vue').default
   }
 }
 </script>
