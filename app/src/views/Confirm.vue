de4398f41c8d296f3cde0e65f53446b1084fb4a5
Kyle <the.big.kb123@gmail.com>
2019-01-19 18:31:45 -0500

    Semi working cancel transaction

--- app/src/views/Confirm.vue
+++ app/src/views/Confirm.vue
@@ -35,69 +35,11 @@
 
   export default {
     name: "confirm",
-    data () {
-      return {
-        type: 'signMessage',
-        msgOrigin: 'unknown',
-        balance: 0,
-        txParams: []
-      }
-    },
     methods: {
       triggerSign: function (event) {
         var bc = new BroadcastChannel('torus_channel');
         bc.postMessage('confirm-transaction');
         window.close();
-
-        // this.hidePopup();
-        // let torusController = window.Vue.TorusUtils.torusController
-        // let state = torusController.getState()
-        // if (Object.keys(state.unapprovedPersonalMsgs).length > 0) {
-        //   let unapprovedPersonalMsgs = []
-        //   console.log(state)
-        //   for (let id in state.unapprovedPersonalMsgs) {
-        //     unapprovedPersonalMsgs.push(state.unapprovedPersonalMsgs[id])
-        //   }
-        //   unapprovedPersonalMsgs = unapprovedPersonalMsgs.sort((a, b) => { return a.time - b.time })
-        //   console.log(unapprovedPersonalMsgs)
-        //   let msgParams = unapprovedPersonalMsgs[0].msgParams
-        //   msgParams.metamaskId = parseInt(unapprovedPersonalMsgs[0].id)
-        //   torusController.signPersonalMessage(msgParams)
-        // } else if (Object.keys(state.unapprovedMsgs).length > 0) {
-        //   let unapprovedMsgs = []
-        //   console.log(state)
-        //   for (let id in state.unapprovedMsgs) {
-        //     unapprovedMsgs.push(state.unapprovedMsgs[id])
-        //   }
-        //   unapprovedMsgs = unapprovedMsgs.sort((a, b) => { return a.time - b.time })
-        //   console.log(unapprovedMsgs)
-        //   let msgParams = unapprovedMsgs[0].msgParams
-        //   msgParams.metamaskId = parseInt(unapprovedMsgs[0].id)
-        //   torusController.signPersonalMessage(msgParams)
-        // } else if (Object.keys(state.unapprovedTypedMessages).length > 0) {
-        //   let unapprovedTypedMessages = []
-        //   console.log(state)
-        //   for (let id in state.unapprovedTypedMessages) {
-        //     unapprovedTypedMessages.push(state.unapprovedTypedMessages[id])
-        //   }
-        //   unapprovedTypedMessages = unapprovedTypedMessages.sort((a, b) => { return a.time - b.time })
-        //   console.log(unapprovedTypedMessages)
-        //   let msgParams = unapprovedTypedMessages[0].msgParams
-        //   msgParams.metamaskId = parseInt(unapprovedTypedMessages[0].id)
-        //   torusController.signPersonalMessage(msgParams)
-        // } else if (Object.keys(state.transactions).length > 0) {
-        //   let transactions = []
-        //   console.log(state)
-        //   for (let id in state.transactions) {
-        //     if (state.transactions[id].status === "unapproved") {
-        //       transactions.push(state.transactions[id])
-        //     }
-        //   }
-        //   console.log(transactions)
-        //   torusController.updateAndApproveTransaction(transactions[0])
-        // } else {
-        //   throw new Error('NO NEW TRANSACTIONS!!!!')
-        // }
       },
       triggerDeny: function (event) {
         var bc = new BroadcastChannel('torus_channel');
@@ -107,26 +49,6 @@
       ...mapActions({
         hidePopup: 'hidePopup'
       })
-    },
-    mounted () {
-      // TODO: implement balance
-      //this.balance = this.$store.state.balance;
-      // this.msgOrigin = document.referrer;
-      // let torusController = window.Vue.TorusUtils.torusController
-      // let state = torusController.getState()
-      // if (Object.keys(state.transactions).length > 0) {
-      //   let transactions = []
-      //   for (let id in state.transactions) {
-      //     if (state.transactions[id].status === "unapproved") {
-      //       transactions.push(state.transactions[id])
-      //     }
-      //   }
-      //   if (transactions.length >= 0) {
-      //     console.log("TRANSACTION", transactions[0]);
-      //     this.txParams = transactions[0].txParams;
-      //     this.type = 'transaction';
-      //   }
-      // }
     }
   }
 </script>
