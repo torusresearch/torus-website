c167d2df8e028dbcc309bd29492ec956781eae02
tanjinyi <tanjinyi96@gmail.com>
2019-01-21 21:38:02 +0800

    Create initial TorusKeyring with SimpleKeyring as base

---
+++ app/src/utils/TorusKeyring.js
@@ -0,0 +1,108 @@
+const EventEmitter = require('events').EventEmitter
+const Wallet = require('ethereumjs-wallet')
+const ethUtil = require('ethereumjs-util')
+const type = 'Simple Key Pair'
+const sigUtil = require('eth-sig-util')
+
+class TorusKeyring extends EventEmitter {
+  constructor (opts) {
+    super()
+    this.type = type
+    this.wallets = []
+    this.deserialize(opts)
+  }
+
+  serialize () {
+    return Promise.resolve(this.wallets.map(w => w.getPrivateKey().toString('hex')))
+  }
+
+  deserialize (privateKeys = []) {
+    return new Promise((resolve, reject) => {
+      try {
+        this.wallets = privateKeys.map((privateKey) => {
+          const stripped = ethUtil.stripHexPrefix(privateKey)
+          const buffer = Buffer.from(stripped, 'hex')
+          const wallet = Wallet.fromPrivateKey(buffer)
+          return wallet
+        })
+      } catch (e) {
+        reject(e)
+      }
+      resolve()
+    })
+  }
+
+  addAccounts (n = 1) {
+    var newWallets = []
+    for (var i = 0; i < n; i++) {
+      newWallets.push(Wallet.generate())
+    }
+    this.wallets = this.wallets.concat(newWallets)
+    const hexWallets = newWallets.map(w => ethUtil.bufferToHex(w.getAddress()))
+    return Promise.resolve(hexWallets)
+  }
+
+  getAccounts () {
+    return Promise.resolve(this.wallets.map(w => ethUtil.bufferToHex(w.getAddress())))
+  }
+
+  // tx is an instance of the ethereumjs-transaction class.
+  signTransaction (address, tx) {
+    const wallet = this._getWalletForAccount(address)
+    var privKey = wallet.getPrivateKey()
+    tx.sign(privKey)
+    return Promise.resolve(tx)
+  }
+
+  // For eth_sign, we need to sign arbitrary data:
+  signMessage (withAccount, data) {
+    const wallet = this._getWalletForAccount(withAccount)
+    const message = ethUtil.stripHexPrefix(data)
+    var privKey = wallet.getPrivateKey()
+    var msgSig = ethUtil.ecsign(Buffer.from(message, 'hex'), privKey)
+    var rawMsgSig = ethUtil.bufferToHex(sigUtil.concatSig(msgSig.v, msgSig.r, msgSig.s))
+    return Promise.resolve(rawMsgSig)
+  }
+
+  // For personal_sign, we need to prefix the message:
+  signPersonalMessage (withAccount, msgHex) {
+    const wallet = this._getWalletForAccount(withAccount)
+    const privKey = ethUtil.stripHexPrefix(wallet.getPrivateKey())
+    const privKeyBuffer = Buffer.from(privKey, 'hex')
+    const sig = sigUtil.personalSign(privKeyBuffer, { data: msgHex })
+    return Promise.resolve(sig)
+  }
+
+  // personal_signTypedData, signs data along with the schema
+  signTypedData (withAccount, typedData) {
+    const wallet = this._getWalletForAccount(withAccount)
+    const privKey = ethUtil.toBuffer(wallet.getPrivateKey())
+    const sig = sigUtil.signTypedData(privKey, { data: typedData })
+    return Promise.resolve(sig)
+  }
+
+  // exportAccount should return a hex-encoded private key:
+  exportAccount (address) {
+    const wallet = this._getWalletForAccount(address)
+    return Promise.resolve(wallet.getPrivateKey().toString('hex'))
+  }
+
+  removeAccount (address) {
+    if (!this.wallets.map(w => ethUtil.bufferToHex(w.getAddress()).toLowerCase()).includes(address.toLowerCase())) {
+      throw new Error(`Address ${address} not found in this keyring`)
+    }
+    this.wallets = this.wallets.filter(w => ethUtil.bufferToHex(w.getAddress()).toLowerCase() !== address.toLowerCase())
+  }
+
+  /* PRIVATE METHODS */
+
+  _getWalletForAccount (account) {
+    const address = sigUtil.normalize(account)
+    let wallet = this.wallets.find(w => ethUtil.bufferToHex(w.getAddress()) === address)
+    if (!wallet) throw new Error('Simple Keyring - Unable to find matching address.')
+    return wallet
+  }
+}
+
+TorusKeyring.type = type
+module.exports = TorusKeyring
