4fdd01b2c0c708de985527e27576bc70bde9cadf
chaitanyapotti <chaitanya@electus.network>
2019-02-20 16:32:54 +0530

    modified vue config

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,15 +1,15 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
   devServer: {
     // open: process.platform === 'darwin',
-    host: 'localhost',
+    host: "localhost",
     port: 3000, // CHANGE YOUR PORT HERE!
-    https: {
-      key: fs.readFileSync(path.resolve('../ssl/server.key')),
-      cert: fs.readFileSync(path.resolve('../ssl/server.crt'))
-    },
+    // https: {
+    //   key: fs.readFileSync(path.resolve('../ssl/server.key')),
+    //   cert: fs.readFileSync(path.resolve('../ssl/server.crt'))
+    // },
     hotOnly: false
   }
-}
+};
