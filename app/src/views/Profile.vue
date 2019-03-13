f6ceca25da277b3ab9df47c3fbed8d178727d704
chaitanyapotti <chaitanya@electus.network>
2019-03-13 14:55:31 +0530

    added transistions

--- app/src/views/Profile.vue
+++ app/src/views/Profile.vue
@@ -42,88 +42,92 @@
             <v-btn color="#75b4fd" :disabled="!valid" class="white--text" v-on:click="sendEth">Send</v-btn>
           </v-form>
         </v-flex>
-        <v-flex xs12>
+        <v-flex xs12 justify-space-around>
           <v-btn color="#75b4fd" class="white--text mb-4" v-on:click="getTokenBalances">Get Token Balances</v-btn>
-          <div v-if="tokenBalances.length > 0 && fetchedTokenBalances">
-            <v-card>
-              <v-card-title>
-                Token Balances
-                <v-spacer />
-                <v-text-field
-                  class="input-width-200 text-lg-right"
-                  v-model="search"
-                  append-icon="search"
-                  label="Search"
-                  single-line
-                  hide-details
-                ></v-text-field>
-              </v-card-title>
-              <v-data-table :headers="headers" :items="tokenBalances" :search="search">
-                <template v-slot:items="props">
-                  <td>{{ props.item.ticker }}</td>
-                  <td class="text-xs-left">
-                    <a :href="props.item.etherscanLink" target="_blank" rel="noreferrer noopener">{{ props.item.name }}</a>
-                  </td>
-                  <td class="text-xs-left">{{ props.item.balance }}</td>
-                  <td class="text-xs-left">
-                    <v-dialog v-model="props.item.dialog" persistent max-width="600px">
-                      <template v-slot:activator="{ on }">
-                        <v-btn color="#75b4fd" v-on="on" class="white--text mb-4">Transfer</v-btn>
-                      </template>
-                      <v-form ref="tokenForm" v-model="tokenFormValid" lazy-validation>
-                        <v-card>
-                          <v-card-title>
-                            <span class="headline">Send Token: {{ props.item.ticker }}</span>
-                          </v-card-title>
-                          <v-card-text>
-                            <v-container grid-list-md>
-                              <v-layout wrap>
-                                <v-flex xs12 sm12 md12>
-                                  <v-text-field
-                                    id="tokenToAddress"
-                                    placeholder="Enter address to send token to"
-                                    aria-label="box"
-                                    solo
-                                    required
-                                    v-model="tokenToAddress"
-                                    :rules="[rules.toAddress, rules.required]"
-                                    height="15px"
-                                    class="input-width"
-                                  ></v-text-field>
-                                </v-flex>
-                                <v-flex xs12 sm12 md12>
-                                  <v-text-field
-                                    id="amount"
-                                    placeholder="Enter token amount to send"
-                                    aria-label="box"
-                                    solo
-                                    required
-                                    v-model="tokenAmount"
-                                    height="15px"
-                                    :rules="[rules.required]"
-                                    class="input-width"
-                                  ></v-text-field>
-                                </v-flex>
-                              </v-layout>
-                            </v-container>
-                          </v-card-text>
-                          <v-card-actions>
-                            <v-btn color="blue darken-1" large flat @click="props.item.dialog = false">Close</v-btn>
-                            <v-spacer></v-spacer>
-                            <v-btn color="blue darken-1" large flat :disabled="!tokenFormValid" @click="onTransferToken(props.item)">Send</v-btn>
-                          </v-card-actions>
-                        </v-card>
-                      </v-form>
-                    </v-dialog>
-                  </td>
-                </template>
-                <v-alert v-slot:no-results :value="true" color="error" icon="warning"> Your search for "{{ search }}" found no results. </v-alert>
-              </v-data-table>
-            </v-card>
-          </div>
-          <div v-else-if="fetchedTokenBalances" class="font-weight-medium">
-            You don't hold any ERC-20 tokens
-          </div>
+          <v-expand-transition>
+            <div v-show="tokenBalances.length > 0 && fetchedTokenBalances">
+              <v-card>
+                <v-card-title>
+                  Token Balances
+                  <v-spacer />
+                  <v-text-field
+                    class="input-width-200 text-lg-right"
+                    v-model="search"
+                    append-icon="search"
+                    label="Search"
+                    single-line
+                    hide-details
+                  ></v-text-field>
+                </v-card-title>
+                <v-data-table :headers="headers" :items="tokenBalances" :search="search">
+                  <template v-slot:items="props">
+                    <td>{{ props.item.ticker }}</td>
+                    <td class="text-xs-left">
+                      <a :href="props.item.etherscanLink" target="_blank" rel="noreferrer noopener">{{ props.item.name }}</a>
+                    </td>
+                    <td class="text-xs-left">{{ props.item.balance }}</td>
+                    <td class="text-xs-left">
+                      <v-dialog v-model="props.item.dialog" persistent max-width="600px">
+                        <template v-slot:activator="{ on }">
+                          <v-btn color="#75b4fd" v-on="on" class="white--text mb-4">Transfer</v-btn>
+                        </template>
+                        <v-form ref="tokenForm" v-model="tokenFormValid" lazy-validation>
+                          <v-card>
+                            <v-card-title>
+                              <span class="headline">Send Token: {{ props.item.ticker }}</span>
+                            </v-card-title>
+                            <v-card-text>
+                              <v-container grid-list-md>
+                                <v-layout wrap>
+                                  <v-flex xs12 sm12 md12>
+                                    <v-text-field
+                                      id="tokenToAddress"
+                                      placeholder="Enter address to send token to"
+                                      aria-label="box"
+                                      solo
+                                      required
+                                      v-model="tokenToAddress"
+                                      :rules="[rules.toAddress, rules.required]"
+                                      height="15px"
+                                      class="input-width"
+                                    ></v-text-field>
+                                  </v-flex>
+                                  <v-flex xs12 sm12 md12>
+                                    <v-text-field
+                                      id="amount"
+                                      placeholder="Enter token amount to send"
+                                      aria-label="box"
+                                      solo
+                                      required
+                                      v-model="tokenAmount"
+                                      height="15px"
+                                      :rules="[rules.required]"
+                                      class="input-width"
+                                    ></v-text-field>
+                                  </v-flex>
+                                </v-layout>
+                              </v-container>
+                            </v-card-text>
+                            <v-card-actions>
+                              <v-btn color="blue darken-1" large flat @click="props.item.dialog = false">Close</v-btn>
+                              <v-spacer></v-spacer>
+                              <v-btn color="blue darken-1" large flat :disabled="!tokenFormValid" @click="onTransferToken(props.item)">Send</v-btn>
+                            </v-card-actions>
+                          </v-card>
+                        </v-form>
+                      </v-dialog>
+                    </td>
+                  </template>
+                  <v-alert v-slot:no-results :value="true" color="error" icon="warning"> Your search for "{{ search }}" found no results. </v-alert>
+                </v-data-table>
+              </v-card>
+            </div>
+          </v-expand-transition>
+          <v-expand-transition>
+            <div v-show="fetchedTokenBalances && tokenBalances.length === 0" class="font-weight-medium">
+              You don't hold any ERC-20 tokens
+            </div>
+          </v-expand-transition>
         </v-flex>
       </v-layout>
     </v-layout>
