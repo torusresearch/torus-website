4ddfeda3c206192878b4ac160f675ee3c7313b10
tetratorus <tetratorus@gmail.com>
2019-05-17 19:28:23 -0400

    add indexes

--- app/src/config.js
+++ app/src/config.js
@@ -6,5 +6,6 @@ export default {
     'https://torus.torusnode.com/jrpc',
     'https://etc.torusnode.com/jrpc'
   ],
+  torusIndexes: [1, 3, 2, 4, 5],
   supportedCurrencies: ['USD', 'AUD', 'CAD', 'EUR', 'GBP', 'HKD', 'IDR', 'JPY', 'KRW', 'RUB', 'SGD', 'UAH']
 }
