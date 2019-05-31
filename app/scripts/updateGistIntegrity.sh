214d3fd947d8167492682d744e56ea86ee80212d
chaitanyapotti <chaitanya.potti@gmail.com>
2019-05-31 12:57:54 +0800

    gist update automation

--- app/scripts/updateGistIntegrity.sh
+++ app/scripts/updateGistIntegrity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://gist.github.com/733405286923fa047af4cb26d167acd4.git ~/gist
-HASH="$(cat ~/torus-website/app/public/embed.min.js | openssl dgst -sha384 -binary | openssl base64 -A)"
+HASH="$(cat ./embed.min.js | openssl dgst -sha384 -binary | openssl base64 -A)"
 cd ~/gist
 echo "<script src="https://app.tor.us/embed.min.js"
         integrity=\"$HASH\"
@@ -9,6 +9,5 @@ echo "<script src="https://app.tor.us/embed.min.js"
 
 git config user.email "chaitanya.potti@gmail.com"
 git config user.name "chaitanyapotti"
-git add .
-git commit -m "Updating embed"
+git commit -am "Updating embed"
 git push
