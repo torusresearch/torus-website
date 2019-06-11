b07f2b9b076c4eb75ddb7f3bb283612d6fa2d222
chaitanyapotti <chaitanya.potti@gmail.com>
2019-06-11 19:44:40 +0800

    fix

--- app/scripts/updateLambda.sh
+++ app/scripts/updateLambda.sh
@@ -11,5 +11,8 @@ zip lambda-code handler.js
 aws lambda update-function-code --function-name torus-routing --publish --zip-file "fileb://lambda-code.zip"
 aws cloudfront get-distribution-config --id $CLOUDFRONT_DISTRIBUTION_ID > cf_config.json
 ETAG=$(cat cf_config.json | grep ETag | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g')
+echo $(ls ~/)
+echo $(ls ~/app/)
+echo $(ls ~/app/app/)
 node ~/app/app/scripts/createUpdatedDistributionConfig.js
 aws cloudfront update-distribution --distribution-config "file://updated_cf_config.json" --id $CLOUDFRONT_DISTRIBUTION_ID --if-match "$ETAG"
