7433356726f97b9a72d0f2eb45b8b3056ae33626
chaitanyapotti <chaitanya.potti@gmail.com>
2019-06-11 20:32:37 +0800

    bucket change

--- app/scripts/uploadToAws.sh
+++ app/scripts/uploadToAws.sh
@@ -4,7 +4,7 @@ PACKAGE_VERSION=$(cat package.json \
   | head -1 \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
-URL=$(echo s3://static.dev.tor.us/"$PACKAGE_VERSION" | tr -d ' ')
+URL=$(echo s3://app.tor.us/"$PACKAGE_VERSION" | tr -d ' ')
 aws s3 rm $URL --recursive
 cd dist
 aws s3 cp . $URL --recursive
