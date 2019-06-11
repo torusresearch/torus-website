a42c62a1b6c507909703ce013bdbedbb4cec5e22
chaitanyapotti <chaitanya.potti@gmail.com>
2019-06-11 20:03:34 +0800

    fix to circleci path

--- app/scripts/updateLambda.sh
+++ app/scripts/updateLambda.sh
@@ -11,5 +11,5 @@ zip lambda-code handler.js
 aws lambda update-function-code --function-name torus-routing --publish --zip-file "fileb://lambda-code.zip"
 aws cloudfront get-distribution-config --id $CLOUDFRONT_DISTRIBUTION_ID > cf_config.json
 ETAG=$(cat cf_config.json | grep ETag | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g')
-node ~/torus-website/app/app/scripts/createUpdatedDistributionConfig.js
+node ~/torus-website/app/scripts/createUpdatedDistributionConfig.js
 aws cloudfront update-distribution --distribution-config "file://updated_cf_config.json" --id $CLOUDFRONT_DISTRIBUTION_ID --if-match "$ETAG"
