b96a1d02da97618cc8e157e800071545516cd291
chaitanyapotti <chaitanya.potti@gmail.com>
2019-06-13 11:07:48 +0800

    lint fixes

--- app/scripts/createUpdatedDistributionConfig.js
+++ app/scripts/createUpdatedDistributionConfig.js
@@ -1,17 +1,17 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 try {
-  const cfConfig = JSON.parse(fs.readFileSync(path.resolve("./cf_config.json"), "utf8"));
-  if (Object.prototype.hasOwnProperty.call(cfConfig, "DistributionConfig")) {
-    const newConfig = cfConfig.DistributionConfig;
+  const cfConfig = JSON.parse(fs.readFileSync(path.resolve('./cf_config.json'), 'utf8'))
+  if (Object.prototype.hasOwnProperty.call(cfConfig, 'DistributionConfig')) {
+    const newConfig = cfConfig.DistributionConfig
     if (newConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items.length > 0) {
-      let arn = newConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items[0].LambdaFunctionARN;
-      const newVersion = parseInt(arn.slice(arn.length - 1), 10) + 1;
-      arn = arn.slice(0, arn.length - 1) + newVersion;
-      newConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items[0].LambdaFunctionARN = arn;
+      let arn = newConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items[0].LambdaFunctionARN
+      const newVersion = parseInt(arn.slice(arn.length - 1), 10) + 1
+      arn = arn.slice(0, arn.length - 1) + newVersion
+      newConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items[0].LambdaFunctionARN = arn
     }
-    fs.writeFileSync(path.resolve("./updated_cf_config.json"), JSON.stringify(newConfig, null, 2), "utf8");
+    fs.writeFileSync(path.resolve('./updated_cf_config.json'), JSON.stringify(newConfig, null, 2), 'utf8')
   }
 } catch (error) {
-  console.log(error);
+  console.log(error)
 }
