7268b74b2f283cb03e0efddd8f86f5300aee0596
chaitanyapotti <chaitanya.potti@gmail.com>
2019-06-18 18:45:34 +0800

    keep fnames

--- app/vue.config.js
+++ app/vue.config.js
@@ -27,6 +27,16 @@ module.exports = {
   css: {
     extract: false
   },
+  configureWebpack: config => {
+    const TerserPlugin = require('terser-webpack-plugin')
+    // Get the current options from the Terser Plugin instance that vue-cli-service added:
+    const options = config.optimization.minimizer[0].options
+    // Set the options you want to set
+    options.terserOptions.keepFnames = true
+    // create a fresh pülugin instance with the new options and
+    // replace the current one with it
+    config.optimization.minimizer[0] = new TerserPlugin(options)
+  },
 
   publicPath: process.env.TORUS_BUILD_ENV === 'production' ? `/${version}/` : '/',
 
