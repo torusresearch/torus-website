6cec7054ad3f20103503d36443208c5342f9eb00
chaitanyapotti <chaitanya.potti@gmail.com>
2019-06-19 14:08:37 +0800

    base routes fix

--- app/src/registerServiceWorker.js
+++ app/src/registerServiceWorker.js
@@ -1,10 +1,9 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker'
-import { version } from '../package.json'
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-  register(`/v${version}/service-worker.js`, {
+  register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
       console.log('App is being served from cache by a service worker.\n' + 'For more details, visit https://goo.gl/AFskqB')
     },
