7076c1713f6fa5b24b6236472f9d4734f124f277
Lionell Briones <llenoil@gmail.com>
2019-06-25 10:32:54 +0800

    Added contents and icons

--- app/src/views/Simplex.vue
+++ app/src/views/Simplex.vue
@@ -1,21 +1,181 @@
 <template>
   <v-flex fill-height class="simplex-page">
-    <v-flex class="simplex-header" style="background: red">
-      <h1>Header</h1>
-    </v-flex>
-    <v-container style="background: blue">
-      <p>Body</p>
+    <v-layout justify-space-between row class="header">
+      <div>
+        <img src="/images/torus_logo.png">
+      </div>
+      <div class="header-title">
+        <img src="/images/wallet_blue_line.svg">
+        <span>{{ username }}'s Wallet</span>
+      </div>
+    </v-layout>
+    <v-container align-content-center>
+      <v-layout column text-sm-center>
+        <v-flex sm6>
+          <h1 class="page-title">Purchase Cryptocurrency with your credit card via Simplex</h1>
+        </v-flex>
+        <v-flex sm6>
+          <p class="page-description">
+            Simplex is a secure way to buy cryptoccurrency with your credit card.
+            <br>Start by entering an amount to get a quote before making your purchase.
+          </p>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row justify-center>
+        <v-flex sm1>
+          <v-subheader>Purchase</v-subheader>
+        </v-flex>
+        <v-flex sm4>
+          <v-text-field
+            class="torus-text-input"
+            persistent-hint
+            hint="Rate : 1 ETH = 260.00 USD"
+            value="0.0000"
+            suffix="ETH"
+            solo
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row justify-center>
+        <v-flex sm1>
+          <v-subheader>Pay</v-subheader>
+        </v-flex>
+        <v-flex sm4>
+          <v-text-field
+            class="torus-text-input"
+            hide-details
+            value="0.00 (Min 50.00)"
+            suffix="USD* "
+            solo
+          ></v-text-field>
+          <div class="v-text-field__details">
+            <div class="v-messages theme--light">
+              <div class="v-messages__wrapper">
+                <div class="v-messages__message">
+                  * Includes 5% Simplex Service Fees
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                      <v-icon color="primary" dark v-on="on">error_outline</v-icon>
+                    </template>
+                    <span>Tooltip</span>
+                  </v-tooltip>
+                </div>
+              </div>
+            </div>
+          </div>
+        </v-flex>
+      </v-layout>
+
+      <v-layout column text-sm-center>
+        <v-flex sm6>
+          <p>
+            <img src="/images/clock-regular.svg">
+            The process would take approximately 10 - 15 mins.
+          </p>
+        </v-flex>
+        <v-flex sm6>
+          <p>
+            <img src="/images/address-card-regular.svg">
+            Please prepare your Identity Card/Passport to complete the purchase.
+          </p>
+        </v-flex>
+      </v-layout>
     </v-container>
+
+    <v-card-actions text-sm-center>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" type="submit">Checkout with Simplex</v-btn>
+      <v-spacer></v-spacer>
+    </v-card-actions>
   </v-flex>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      username: "Username"
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .simplex-page {
   background: url("/images/footer_waves.png") no-repeat;
-  background-position: center bottom; 
+  background-position: center bottom;
+
+  .header {
+    padding: 45px 50px 30px;
+  }
+
+  .header-title {
+    display: flex;
+    img {
+      margin-right: 20px;
+    }
+    span {
+      font-size: 25px;
+    }
+  }
+
+  .page-title {
+    font-size: 25px;
+    line-height: 30px;
+    margin-bottom: 15px;
+  }
+
+  .page-description {
+    font-size: 17px;
+    line-height: 21px;
+    margin-bottom: 50px;
+  }
+
+  .torus-text-input.v-text-field--solo {
+    font-size: 20px;
+
+    & /deep/ .v-input__slot {
+      box-shadow: none;
+      margin-bottom: 5px;
+      border-radius: 5px;
+      background-image: linear-gradient(to right, #5495f7, #295dab);
+      padding: 2px;
+    }
+    & /deep/ .v-text-field__slot {
+      background-color: rgba(255, 255, 255, 0.3);
+      border-radius: 4px;
+      height: 44px;
+      input {
+        text-align: center;
+        color: #ffffff;
+      }
+    }
+    & /deep/ .v-text-field__suffix {
+      background-color: #295dab;
+      height: 100%;
+      line-height: 42px;
+      color: #ffffff;
+      padding: 0 15px;
+      min-width: 90px;
+      text-align: center;
+    }
+  }
+
+  & /deep/ .v-text-field__details {
+    margin-bottom: 25px;
+    padding: 0 12px;
+  }
+}
+
+@media only screen and (min-width: 1264px) {
+  .simplex-page {
+    background-size: 100%;
+  }
+
+  .container {
+    max-width: 1185px;
+  }
 }
 </style>
