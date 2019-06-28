7c6158b19974df95dfdc7fd366ba1f893688dc10
Lionell Briones <llenoil@gmail.com>
2019-06-28 13:01:51 +0800

    Added layout for the tabs

--- app/src/containers/WalletAccountsHome.vue
+++ app/src/containers/WalletAccountsHome.vue
@@ -6,9 +6,19 @@
         <span class="text-bluish headline"> My Accounts</span>
       </span>
     </v-flex>
-    <router-link :to="{ name: 'walletAccounts' }">Account Management</router-link>
-    <router-link :to="{ name: 'walletAccountImport' }">Import</router-link>
-    <router-link :to="{ name: 'walletAccountExport' }">Export</router-link>
+    <v-flex xs12 sm8 mt-3>
+      <v-layout row wrap align-content-space-between>
+        <v-flex xs4 px-1>
+          <v-btn block depressed class="grey lighten-3 account-tab" :to="{ name: 'walletAccountsHome' }">Account Management</v-btn>
+        </v-flex>
+        <v-flex xs4 px-1>
+          <v-btn block depressed class="grey lighten-3 account-tab" :to="{ name: 'walletAccountImport' }">Import</v-btn>
+        </v-flex>
+        <v-flex xs4 px-1>
+          <v-btn block depressed class="grey lighten-3 account-tab" :to="{ name: 'walletAccountExport' }">Export</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-flex>    
     <router-view></router-view>
     <v-flex xs12>
       <v-layout row wrap>
@@ -26,4 +36,8 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.account-tab:hover {
+  transform: scale(1.05);
+}
+</style>
