0de70c7e1d1257661a9db99796bd9b977492af51
Lastresmoya <lastresmoyallc@gmail.com>
2019-07-04 13:09:39 -0600

    wip: updating design of wallethome view

--- app/src/plugins/vuetify.js
+++ app/src/plugins/vuetify.js
@@ -4,7 +4,7 @@ import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#3f51b5',
+    primary: '#3996ff',
     secondary: '#b0bec5',
     accent: '#8c9eff',
     error: '#b71c1c',
--- app/src/views/Wallet.vue
+++ app/src/views/Wallet.vue
@@ -1,27 +1,3 @@
-<template>
-  <v-container fill-height>
-    <v-layout row wrap justify-center fill-height align-content-start>
-      <v-flex d-flex xs12>
-        <v-tabs fixed-tabs v-model="selectedItem" grow :color="$vuetify.theme.torus_bcg">
-          <header-item
-            v-for="headerItem in headerItems"
-            :key="headerItem.name"
-            :isSelected="selectedItem === headerItem.route"
-            :href="`#${headerItem.name}`"
-            :icon="headerItem.icon"
-            :to="headerItem.route"
-          >
-            {{ headerItem.display }}
-          </header-item>
-        </v-tabs>
-      </v-flex>
-      <v-flex xs12>
-        <router-view></router-view>
-      </v-flex>
-    </v-layout>
-  </v-container>
-</template>
-
 <script>
 import HeaderItem from '../components/HeaderItem.vue'
 
@@ -33,15 +9,50 @@ export default {
     return {
       selectedItem: 'home',
       headerItems: [
-        { name: 'home', icon: 'home', display: 'Home', route: '/wallet/home' },
-        { name: 'history', icon: 'history', display: 'History', route: '/wallet/history' },
-        { name: 'settings', icon: 'settings', display: 'Settings', route: '/wallet/settings' },
-        { name: 'accounts', icon: 'account_circle', display: 'Accounts', route: '/wallet/accounts' }
+        { name: 'home', display: 'Home', route: '/wallet/home' },
+        { name: 'transfer', display: 'Transfer', route: '/wallet/transfer' },
+        { name: 'top-up', display: 'Top-up', route: '/wallet/top-up' },
+        { name: 'activity', display: 'Activity', route: '/wallet/history' },
+        { name: 'settings', display: 'Settings', route: '/wallet/settings' }
       ]
+      // headerItems: [
+      //   { name: 'home', display: 'Home', route: '/wallet/home' },
+      //   { name: 'history', display: 'History', route: '/wallet/history' },
+      //   { name: 'settings', display: 'Settings', route: '/wallet/settings' },
+      //   { name: 'accounts', display: 'Accounts', route: '/wallet/accounts' }
+      // ]
     }
   }
 }
 </script>
+
+<template>
+  <v-container fill-height>
+    <v-layout row wrap justify-center fill-height align-content-start>
+      <v-flex class="mb-4">
+        <img :src="require('../../public/images/torus_logo.png')" />
+      </v-flex>
+      <v-flex d-flex xs12>
+        <v-flex fixed-tabs v-model="selectedItem" grow :color="$vuetify.theme.torus_bcg">
+          <header-item
+            v-for="headerItem in headerItems"
+            :key="headerItem.name"
+            :isSelected="selectedItem === headerItem.route"
+            :href="`#${headerItem.name}`"
+            :to="headerItem.route"
+          >
+            <span class="font-weight-bold">
+              {{ headerItem.display }}
+            </span>
+          </header-item>
+        </v-flex>
+      </v-flex>
+      <v-flex xs12>
+        <router-view></router-view>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
 
 <style lang="scss">
 body {
