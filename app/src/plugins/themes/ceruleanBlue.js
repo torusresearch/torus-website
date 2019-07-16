463f316aa58c42aa530ffcb7eb49745ba689768c
Lionell Briones <llenoil@gmail.com>
2019-07-16 13:36:35 +0800

    Updated theme colors

--- app/src/plugins/themes/ceruleanBlue.js
+++ app/src/plugins/themes/ceruleanBlue.js
@@ -1,14 +1,16 @@
 export default {
   primary: '#295dab',
+  error: '#e20d0d',
+  torus_accept: '#2dcc70',
+  torus_bcg: '#f7f7f7',
+  torus_text: '#0F1222',
+  // Unused
+  torus_svg_bcg: '#5495f7',
+  torus_blue: '#5495f7',
   secondary: '#b0bec5',
   accent: '#8c9eff',
-  error: '#b71c1c',
-  torus_accept: '#56ab7f',
   torus_reject: '#959595',
   torus_reject_mild: '#959595',
   torus_active: '#7d7c7e',
-  torus_svg_bcg: '#3996ff',
-  torus_blue: '#3996ff',
-  torus_bcg: '#f7f7f7',
   torus_icon: '#cccaca'
 }
