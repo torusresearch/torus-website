50ca91b32642dd39adc2807a9f32ebbd90a18ef8
chaitanyapotti <chaitanya.potti@gmail.com>
2019-08-02 07:42:34 +0530

    tags fix

--- app/scripts/gitPush.sh
+++ app/scripts/gitPush.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 git config user.email "chaitanya.potti@gmail.com"
 git config user.name "chaitanyapotti"
-if ! (git diff --quiet && git diff --staged --quiet); then
-    git add package.json
-    git commit -m "Updating version [skip ci]"
-    if (git push origin $CIRCLE_BRANCH); then 
-        if [[ "$CIRLCE_BRANCH" = 'master' ]]; then
-            git push --tags 
-        fi
+if ! (git diff --quiet && git diff --staged --quiet && git diff origin/$CIRCLE_BRANCH HEAD --quiet); then
+    git push origin $CIRCLE_BRANCH
+    if [[ "$CIRLCE_BRANCH" = 'master' ]]; then
+        git push --tags 
     fi
 fi
