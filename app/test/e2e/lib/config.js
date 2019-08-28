9aa5728c8d571da7d34f5f5ef8e4351532fe2a20
Lionell Briones <llenoil@gmail.com>
2019-08-28 14:34:30 +0800

    Added mobile testing config

--- app/test/e2e/lib/config.js
+++ app/test/e2e/lib/config.js
@@ -1,3 +1,5 @@
+const isMobile = process.env.MOBILE_ENV || false
+
 module.exports = {
   testAccountName: 'toruspuppeteer@gmail.com',
   testAccountPassword: 'toruse2e',
@@ -8,6 +10,7 @@ module.exports = {
   isDevTools: true,
   launchTimeout: 10000,
   waitingTimeout: 40000,
-  viewportWidth: 1200,
-  viewportHeight: 900
+  isMobile: isMobile,
+  viewportWidth: isMobile ? 425 : 1200,
+  viewportHeight: isMobile ? 650 : 900
 }
