4d888364ac46e125cdf0eb8e18d751cb5b465480
Lionell Briones <llenoil@gmail.com>
2019-09-04 16:42:07 +0800

    Added light backgrounds

--- app/src/plugins/themes/darkTheme.js
+++ app/src/plugins/themes/darkTheme.js
@@ -1,8 +1,8 @@
 export default {
   background_body: '#242529',
   background: '#2F3136;',
-  background_2: '#eef2f4',
-  background_3: '#e8edef',
+  background_2: '#585a5e',
+  background_3: '#828386',
   disabled: '#403F43',
   primary: '#0364FF'
 }
