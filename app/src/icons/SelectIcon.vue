f79058e71183c0655dfd7919abaa5c218183123d
Lionell Briones <llenoil@gmail.com>
2019-09-24 11:50:30 +0800

    Added new icons

--- app/src/icons/SelectIcon.vue
+++ app/src/icons/SelectIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24">
-    <path fill="currentColor" d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+  <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="currentColor" d="M7,10L12,15L17,10H7Z" />
   </svg>
 </template>
