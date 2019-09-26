062b2638d3932d457f42c3860ab066b27c25ffc5
chaitanyapotti <chaitanya.potti@gmail.com>
2019-09-26 11:12:11 +0800

    clean up torus website

--- app/src/views/Popup/Popup.vue
+++ app/src/views/Popup/Popup.vue
@@ -2,19 +2,7 @@
   <v-container fluid>
     <v-layout wrap>
       <v-flex xs12 sm6 md3>
-        <v-text-field
-          id="email"
-          placeholder="Enter your email address"
-          aria-label="box"
-          solo
-          type="email"
-          v-model="email"
-          :rules="[rules.email]"
-          height="15px"
-        ></v-text-field>
-      </v-flex>
-      <v-flex xs12 sm6 md3>
-        <v-btn color="#75b4fd" class="white--text" @click="triggerLogin({ calledFromEmbed: false })" id="googleAuthBtnf">Google</v-btn>
+        Welcome to Torus
       </v-flex>
     </v-layout>
   </v-container>
@@ -26,28 +14,7 @@ import { mapActions } from 'vuex'
 export default {
   name: 'popup',
   data: function() {
-    return {
-      torusNodeEndpoints: [
-        'https://binancelabs.torusnode.com/jrpc',
-        'https://waseda.torusnode.com/jrpc',
-        'https://vgr.torusnode.com/jrpc',
-        'https://torus.torusnode.com/jrpc',
-        'https://etc.torusnode.com/jrpc'
-      ],
-      email: '',
-      rules: {
-        email: value => {
-          // eslint-disable-next-line max-len
-          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          return pattern.test(value) || 'Invalid e-mail.'
-        }
-      }
-    }
-  },
-  methods: {
-    ...mapActions({
-      triggerLogin: 'triggerLogin'
-    })
+    return {}
   },
   mounted() {
     const googleInterval = setInterval(() => {
