c9644a0a82d86de09ed941597bc2eb38286a2b01
Lionell Briones <llenoil@gmail.com>
2019-10-03 12:06:04 +0300

    Updated ui for event card, refresh, and currency

--- app/src/components/WalletHome/PromotionCard/PromotionCard.vue
+++ app/src/components/WalletHome/PromotionCard/PromotionCard.vue
@@ -2,15 +2,20 @@
   <v-card class="card-shadow">
     <v-card-text class="pt-0 px-6" :class="$vuetify.breakpoint.lgAndUp ? 'pb-2' : 'pb-3'">
       <v-layout>
-        <v-flex class="text_1--text pt-6" :class="$vuetify.breakpoint.xsOnly ? 'xs12' : $vuetify.breakpoint.lgAndUp ? 'xs8' : 'xs9'">
+        <v-flex class="text_1--text pt-6" :class="$vuetify.breakpoint.xsOnly ? 'text-center xs12' : $vuetify.breakpoint.lgAndUp ? 'xs8' : 'xs9'">
           <div class="body-1 font-weight-bold">{{ title }}</div>
           <slot name="subtitle">
             <div :class="$vuetify.breakpoint.lgAndUp ? 'body-2' : 'caption'">{{ subtitle }}</div>
+            <div>
+              <v-btn color="primary" depressed class="px-12 py-1 mt-4 white--text" :href="detailsLink" target="_blank">
+                {{ detailsText }}
+              </v-btn>
+            </div>
           </slot>
         </v-flex>
         <slot name="image">
           <v-flex xs4 pt-4 class="text-right hidden-xs-only">
-            <img :src="require(`../../../../public/images/${imagePath}`)" style="height: 100px" />
+            <img :src="require(`../../../../public/images/${imagePath}`)" style="height: 101px" />
           </v-flex>
         </slot>
       </v-layout>
@@ -20,6 +25,6 @@
 
 <script>
 export default {
-  props: ['title', 'subtitle', 'imagePath']
+  props: ['title', 'subtitle', 'imagePath', 'detailsLink', 'detailsText']
 }
 </script>
