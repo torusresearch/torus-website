3391aa646f41baa3bdb44940e43ef33b9f231063
Lionell Briones <llenoil@gmail.com>
2019-10-07 05:29:12 +0300

    Added dummy data for collectibles list

--- app/src/components/WalletHome/CollectiblesList/CollectiblesList.vue
+++ app/src/components/WalletHome/CollectiblesList/CollectiblesList.vue
@@ -1,11 +1,11 @@
 <template>
   <v-layout wrap align-center>
-    <v-flex class="xs12 sm6 md4 lg3 px-4 my-4" v-for="n in 5" :key="n">
-      <v-card class="card-shadow py-4" @click="loadCollectible(n)">
-        <v-list-item router-link :to="{ name: 'walletHomeCollectible' }">
-          <v-list-item-content class="subtitle-1 font-weight-bold">Proof of Attendance Protocol</v-list-item-content>
+    <v-flex class="xs12 sm6 md4 lg3 px-4 my-4" v-for="contract in contracts" :key="contract.id">
+      <v-card class="card-shadow py-4">
+        <v-list-item router-link :to="{ name: 'walletHomeCollectible', query: { id: contract.id } }">
+          <v-list-item-content class="subtitle-1 font-weight-bold">{{ contract.name }}</v-list-item-content>
           <v-list-item-avatar size="100">
-            <v-img :src="require(`../../../../public/images/kitty-eth.svg`)"></v-img>
+            <v-img :src="contract.image"></v-img>
           </v-list-item-avatar>
         </v-list-item>
       </v-card>
@@ -15,9 +15,25 @@
 
 <script>
 export default {
-  methods: {
-    loadCollectible(selectedCollectible) {
-      this.selectedCollectible = selectedCollectible
+  computed: {
+    contracts() {
+      return [
+        {
+          id: 1,
+          name: 'Cryptokitties',
+          image: 'https://www.cryptokitties.co/images/kitty-eth.svg'
+        },
+        {
+          id: 2,
+          name: 'My Crypto Heroes',
+          image: 'https://pbs.twimg.com/profile_images/1074160618788147200/W-COgBLA_400x400.jpg'
+        },
+        {
+          id: 3,
+          name: 'Proof of Attendance Protocol',
+          image: 'https://cdn.stateofthedapps.com/dapps/poap/logo_poap_2e95b0adb2b95625bcd5240c61c74b8d1037c29aed9d3f2b6e4d9c1fb6cebdc3_opti.png'
+        }
+      ]
     }
   }
 }
--- app/src/containers/WalletHome/WalletHomeCollectible/WalletHomeCollectible.vue
+++ app/src/containers/WalletHome/WalletHomeCollectible/WalletHomeCollectible.vue
@@ -1,32 +1,42 @@
 <template>
   <div>
-    <v-breadcrumbs class="px-4" :items="items">
+    <v-breadcrumbs class="px-4" :items="breadcrumb">
       <template v-slot:divider>
         <v-icon>$vuetify.icons.page_next</v-icon>
       </template>
     </v-breadcrumbs>
-    <v-layout wrap align-center>
+    <v-layout wrap align-end>
       <v-flex xs6 px-4>
-        <v-select :items="items" outlined hide-details append-icon="$vuetify.icons.select"></v-select>
+        <v-select
+          :items="contracts"
+          v-model="selectedContract"
+          @change="changeContract"
+          item-text="name"
+          item-value="id"
+          outlined
+          hide-details
+          append-icon="$vuetify.icons.select"
+          return-object
+        ></v-select>
       </v-flex>
-      <v-flex xs6 px-4 class="body-2 text_2--text">Something</v-flex>
+      <v-flex xs6 px-4 pb-1 class="body-2 text_2--text text-capitalize">{{ platform }}</v-flex>
     </v-layout>
     <v-layout wrap align-center mt-10>
-      <v-flex xs6 sm3 md2 px-4 pb-4 v-for="i in 10" :key="i">
+      <v-flex xs6 sm3 md2 px-4 pb-4 v-for="asset in selectedContract.assets" :key="asset.id">
         <v-card class="mx-auto asset" max-width="344">
-          <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="140px"></v-img>
+          <v-img :src="asset.image_url" height="140px"></v-img>
           <v-card-text class="asset-text py-1 px-3">
-            <div class="body-2">Nisse Fabears</div>
+            <div class="body-2">{{ asset.name }}</div>
             <div class="text-right asset-details mt-1">
-              <div class="font-weight-medium">0.25 ETH</div>
-              <div class="font-weight-light text_2--text">$49.38 USD</div>
+              <div class="font-weight-medium">{{ asset.costEth }}</div>
+              <div class="font-weight-light text_2--text">{{ asset.costCurrency }}</div>
             </div>
           </v-card-text>
         </v-card>
       </v-flex>
       <v-flex xs6 sm3 md2 px-4 pb-4>
         <v-card class="mx-auto asset asset--new" max-width="344">
-          <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="140px">
+          <v-img src="https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/1713976.png" height="140px">
             <v-card-title class="flex-column justify-end primary--text fill-height">
               <div class="add-container text-center">
                 <v-icon x-large class="primary--text">$vuetify.icons.add</v-icon>
@@ -51,26 +61,87 @@
 export default {
   data() {
     return {
-      items: [
+      breadcrumb: [
         {
-          text: 'Dashboard',
+          text: 'Home',
           disabled: false,
-          href: 'breadcrumbs_dashboard'
+          exact: true,
+          to: '/wallet/home'
         },
         {
-          text: 'Link 1',
+          text: 'Collectibles',
           disabled: false,
-          href: 'breadcrumbs_link_1'
+          exact: true,
+          to: '/wallet/home/collectible?id=1'
         },
         {
-          text: 'Link 2',
-          disabled: true,
-          href: 'breadcrumbs_link_2'
+          text: '',
+          disabled: true
+        }
+      ],
+      selectedContract: ''
+    }
+  },
+  computed: {
+    platform() {
+      return 'Ethereum Blockchain'
+    },
+    contracts() {
+      return [
+        {
+          id: 1,
+          name: 'Cryptokitties',
+          image: 'https://www.cryptokitties.co/images/kitty-eth.svg',
+          assets: [
+            {
+              name: 'Jack',
+              image_url: 'https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/1713976.png',
+              costEth: '0.0498 ETH',
+              costCurrency: '$8.65 USD',
+              color: 'dahlia'
+            },
+            {
+              name: 'Jack',
+              image_url: 'https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/1712114.png',
+              costEth: '0.0498 ETH',
+              costCurrency: '$8.65 USD',
+              color: 'dahlia'
+            },
+            {
+              name: 'Jack',
+              image_url: 'https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/1712114.png',
+              costEth: '0.0498 ETH',
+              costCurrency: '$8.65 USD',
+              color: 'dahlia'
+            }
+          ]
+        },
+        {
+          id: 2,
+          name: 'My Crypto Heroes',
+          image: 'https://pbs.twimg.com/profile_images/1074160618788147200/W-COgBLA_400x400.jpg'
+        },
+        {
+          id: 3,
+          name: 'Proof of Attendance Protocol',
+          image: 'https://cdn.stateofthedapps.com/dapps/poap/logo_poap_2e95b0adb2b95625bcd5240c61c74b8d1037c29aed9d3f2b6e4d9c1fb6cebdc3_opti.png'
         }
       ]
     }
   },
-  methods: {}
+  methods: {
+    changeContract() {
+      this.breadcrumb[2].text = this.selectedContract.name
+      this.breadcrumb[2].href = `collectible/${this.selectedContract.id}`
+    }
+  },
+  created() {
+    const contractId = parseInt(this.$route.query.id || 1)
+    this.selectedContract = this.contracts.find(contract => {
+      return contract.id === contractId
+    })
+    this.changeContract()
+  }
 }
 </script>
 
