4f242247e328966f24910a7d9f01033371afa6f0
chaitanyapotti <chaitanya.potti@gmail.com>
2019-10-18 11:02:46 +0800

    change google password

--- app/test/e2e/lib/config.js
+++ app/test/e2e/lib/config.js
@@ -2,7 +2,7 @@ const isMobile = process.env.MOBILE_ENV || false
 
 module.exports = {
   testAccountName: 'toruspuppeteer@gmail.com',
-  testAccountPassword: 'toruse2e',
+  testAccountPassword: 'torus123@',
   testPrivateKey: '7a7c34d65fb8dfae2c2b8668a8443526af8cb8d76efdb9815fa5fece59bb524b',
   testEthAddress: '0x7574d3b436e89B42Fc3418eB188179265CE247AD',
   baseUrl: 'https://localhost:3000', // https://dev.to
