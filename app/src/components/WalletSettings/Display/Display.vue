47006425b2d53a8d4a87fb13ecc280240c62918d
Lionell Briones <llenoil@gmail.com>
2019-11-01 11:44:57 +0300

    - Updated saving of theme - Fixed Add contact UI - Handle collectible assets without name

--- app/src/components/WalletSettings/Display/Display.vue
+++ app/src/components/WalletSettings/Display/Display.vue
@@ -1,11 +1,29 @@
 <template>
-  <div :class="$vuetify.breakpoint.xsOnly ? '' : 'py-4 px-12'">
+  <div class="select-theme-container" :class="$vuetify.breakpoint.xsOnly ? '' : 'py-4 px-12'">
     <div class="body-2 text_1--text mb-1 px-1">Select Theme</div>
     <v-layout wrap>
-      <v-flex xs12 sm4 px-1 mb-1 v-for="theme in themes" :key="`${theme.name}`">
-        <v-btn @click="selectTheme(theme.name)" depressed block class="theme-btn" :class="`${theme.name}-color`" :style="themeBtnStyle(theme)">
-          {{ theme.label }}
-        </v-btn>
+      <v-flex xs12 px-1 mb-1>
+        <v-menu class="" transition="slide-y-transition" bottom>
+          <template v-slot:activator="{ on }">
+            <v-chip class="select-theme" :style="themeOptionStyle(selectedTheme)" label outlined large v-on="on">
+              <span>{{ selectedTheme ? selectedTheme.label : 'Select a theme' }}</span>
+              <div class="flex-grow-1 text-right pr-2">
+                <v-icon right>$vuetify.icons.select</v-icon>
+              </div>
+            </v-chip>
+          </template>
+          <v-list class="select-theme-list pa-0">
+            <v-list-item
+              @click="selectedTheme = theme"
+              :style="themeOptionStyle(theme)"
+              class="select-theme-item"
+              v-for="theme in themes"
+              :key="`${theme.name}`"
+            >
+              {{ theme.label }}
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </v-flex>
     </v-layout>
     <v-flex class="pt-4 text-right">
@@ -28,38 +46,43 @@ export default {
   data() {
     return {
       themes: themes,
+      selectedTheme: '',
       snackbar: false,
       snackbarText: '',
       snackbarColor: 'success'
     }
   },
   methods: {
-    selectTheme(themeName) {
-      this.$store.dispatch('setTheme', themeName)
-    },
     saveTheme() {
       this.$store
-        .dispatch('setUserTheme')
+        .dispatch('setUserTheme', this.selectedTheme.name)
         .then(() => {
+          this.selectedTheme = ''
           this.snackbar = true
           this.snackbarColor = 'success'
           this.snackbarText = 'Successfully saved theme'
         })
         .catch(err => {
+          this.selectedTheme = ''
           this.snackbar = true
           this.snackbarColor = 'error'
           this.snackbarText = err
         })
     },
-    themeBtnStyle(theme) {
-      return {
-        color: theme.theme.primary,
-        backgroundColor: theme.theme.background_body_1,
-        borderColor: theme.theme.primary,
-        borderWidth: '1px',
-        borderStyle: 'solid'
-      }
+    themeOptionStyle(theme) {
+      if (theme)
+        return {
+          color: `${theme.theme.primary} !important`,
+          backgroundColor: `${theme.theme.background_body_1} !important`,
+          borderColor: theme.theme.primary,
+          borderWidth: '1px',
+          borderStyle: 'solid'
+        }
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import 'Display.scss';
+</style>
