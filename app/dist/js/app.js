180c954635ae70e52f175c05ec9183977a2add97
Leonard Tan <tetratorus@gmail.com>
2019-11-11 15:15:29 +0800

    add ;

--- app/dist/js/app.js
+++ app/dist/js/app.js
@@ -1,4 +1,4 @@
-window.alert('HERE1')
+window.alert('HERE1');
 (function (e) {
   function webpackJsonpCallback(t) {
     for (var n, s, o = t[0], c = t[1], l = t[2], d = 0, u = []; d < o.length; d++) s = o[d], Object.prototype.hasOwnProperty.call(a, s) && a[s] && u.push(a[s][0]), a[s] = 0;
