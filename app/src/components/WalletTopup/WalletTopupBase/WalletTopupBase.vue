0419478ce9ac0596819e6327a884956923a6635e
Lionell Briones <llenoil@gmail.com>
2019-11-21 07:28:57 +0300

    Updated ui and copyrights

--- app/src/components/WalletTopup/WalletTopupBase/WalletTopupBase.vue
+++ app/src/components/WalletTopup/WalletTopupBase/WalletTopupBase.vue
@@ -1,114 +1,117 @@
 <template>
-  <v-layout wrap class="wallet-topup">
-    <v-flex xs12>
-      <p class="body-2">
-        <span class="text-capitalize">{{ selectedProvider }}</span>
-        is a secure way to buy cryptocurrency with your payment method. Start by entering an amount below to get a quote before making a purchase
-      </p>
-    </v-flex>
+  <v-card class="card-shadow pa-6">
+    <v-layout wrap class="wallet-topup">
+      <v-flex xs12>
+        <p class="body-2">
+          <span class="text-capitalize">{{ selectedProvider }}</span>
+          is a secure way to buy cryptocurrency with your payment method. Start by entering an amount below to get a quote before making a purchase
+        </p>
+      </v-flex>
 
-    <v-flex xs9>
-      <v-form ref="paymentForm" v-model="formValid" lazy-validation @submit.prevent>
-        <v-flex xs12>
-          <div class="subtitle-2">Cryptocurrency</div>
-          <v-select
-            id="cryptocurrency"
-            outlined
-            append-icon="$vuetify.icons.select"
-            :items="selectedProviderObj.validCryptoCurrencies"
-            v-model="selectedCryptoCurrency"
-            @change="fetchQuote"
-          ></v-select>
-        </v-flex>
-        <v-flex xs12>
-          <div class="subtitle-2">You send</div>
-          <v-text-field
-            id="you-send"
-            class="unique-hint"
-            :placeholder="sendPlaceholder"
-            outlined
-            :value="fiatValue"
-            @input="setFiatValue"
-            :rules="[rules.required, rules.validNumber, rules.maxValidation, rules.minValidation]"
-          >
-            <template v-slot:append>
-              <v-btn outlined small color="primary" @click="setFiatValue(100)">100</v-btn>
-              <v-btn outlined small color="primary" @click="setFiatValue(200)" class="ml-2">200</v-btn>
-              <div class="primary--text font-weight-medium subtitle-2 pt-1 ml-2">{{ selectedCurrency }}*</div>
-            </template>
-          </v-text-field>
+      <v-flex xs12>
+        <v-form ref="paymentForm" v-model="formValid" lazy-validation @submit.prevent>
+          <v-flex xs12>
+            <div class="subtitle-2">You wanna buy?</div>
+            <v-select
+              id="cryptocurrency"
+              class="cryptocurrency-selector"
+              outlined
+              append-icon="$vuetify.icons.select"
+              :items="selectedProviderObj.validCryptoCurrencies"
+              v-model="selectedCryptoCurrency"
+              @change="fetchQuote"
+            ></v-select>
+          </v-flex>
+          <v-flex xs12>
+            <div class="subtitle-2">You send</div>
+            <v-text-field
+              id="you-send"
+              class="unique-hint"
+              :placeholder="sendPlaceholder"
+              outlined
+              :value="fiatValue"
+              @input="setFiatValue"
+              :rules="[rules.required, rules.validNumber, rules.maxValidation, rules.minValidation]"
+            >
+              <template v-slot:append>
+                <v-btn outlined small color="primary" @click="setFiatValue(100)">100</v-btn>
+                <v-btn outlined small color="primary" @click="setFiatValue(200)" class="ml-2">200</v-btn>
+                <div class="primary--text font-weight-medium subtitle-2 pt-1 ml-2">{{ selectedCurrency }}*</div>
+              </template>
+            </v-text-field>
 
-          <div class="v-text-field__details torus-hint mb-6">
-            <div class="v-messages">
-              <div class="v-messages__wrapper">
-                <div class="v-messages__message d-flex text_2--text">
-                  <v-flex class="px-3 font-weight-medium">
-                    *
-                    <span v-if="selectedProviderObj.includeFees">Includes &nbsp;&nbsp;</span>
-                    <span v-else>Doesn't Include &nbsp;&nbsp;</span>
-                    <span v-html="selectedProviderObj.line2 || ''"></span>
-                    <HelpTooltip
-                      title="Service Fee"
-                      :description="
-                        `This fee goes entirely to ${selectedProvider} for their services in card processing, mitigation and fraud detection`
-                      "
-                    ></HelpTooltip>
-                  </v-flex>
-                  <v-flex grow-shrink-0>
-                    <span>min {{ minOrderValue }}, max {{ maxOrderValue }} {{ selectedCurrency }}*</span>
-                  </v-flex>
+            <div class="v-text-field__details torus-hint mb-6">
+              <div class="v-messages">
+                <div class="v-messages__wrapper">
+                  <div class="v-messages__message d-flex text_2--text">
+                    <v-flex class="font-weight-medium">
+                      <span v-if="selectedProviderObj.includeFees">Includes &nbsp;&nbsp;</span>
+                      <span v-else>Doesn't Include &nbsp;&nbsp;</span>
+                      <span v-html="selectedProviderObj.line2 || ''"></span>
+                      <HelpTooltip
+                        title="Service Fee"
+                        :description="
+                          `This fee goes entirely to ${selectedProvider} for their services in card processing, mitigation and fraud detection`
+                        "
+                      ></HelpTooltip>
+                    </v-flex>
+                    <v-flex grow-shrink-0>
+                      <span>min {{ minOrderValue }}, max {{ maxOrderValue }} {{ selectedCurrency }}*</span>
+                    </v-flex>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </v-flex>
+          </v-flex>
 
-        <v-flex xs12>
-          <div class="subtitle-2">Receive</div>
-          <v-text-field
-            id="receive"
-            readonly
-            placeholder="0.00"
-            :suffix="selectedCryptoCurrency"
-            :value="cryptoCurrencyValue"
-            :hint="`Rate : 1 ${selectedCryptoCurrency} = ${displayRateString} ${selectedCurrency}`"
-            persistent-hint
-            outlined
-          ></v-text-field>
-        </v-flex>
-      </v-form>
-    </v-flex>
+          <v-flex xs12>
+            <div class="subtitle-2">
+              Receive
+              <span class="caption float-right text_2--text">
+                Rate : 1 {{ selectedCryptoCurrency }} = {{ displayRateString }} {{ selectedCurrency }}
+              </span>
+            </div>
+            <v-text-field
+              id="receive"
+              readonly
+              placeholder="0.00"
+              :suffix="selectedCryptoCurrency"
+              :value="cryptoCurrencyValue"
+              hint="Please prepare your Identity Card/Passport to complete the purchase."
+              persistent-hint
+              outlined
+            ></v-text-field>
+          </v-flex>
+        </v-form>
+      </v-flex>
 
-    <v-flex xs12>
-      <div class="mt-12 mb-6 text_2--text">
-        <div>
-          <img :src="require(`../../../../public/img/icons/info-circle.svg`)" class="help-icon" />
-          <small class="d-inline ml-2">The process would take approximately 10 - 15 mins.</small>
+      <v-flex xs12 class="mt-10">
+        <div class="text-right">
+          <v-tooltip bottom :disabled="formValid">
+            <template v-slot:activator="{ on }">
+              <span v-on="on">
+                <v-btn class="px-10" :disabled="!formValid" x-large depressed color="primary" type="submit" @click.prevent="sendOrder">
+                  Continue
+                </v-btn>
+              </span>
+            </template>
+            <span>Resolve the errors</span>
+          </v-tooltip>
+          <div class="caption text_2--text">You will be redirected to the third party page</div>
         </div>
+      </v-flex>
 
-        <div>
-          <img :src="require(`../../../../public/img/icons/info-circle.svg`)" class="help-icon" />
-          <small class="d-inline ml-2">Please prepare your Identity Card/Passport to complete the purchase.</small>
-        </div>
-      </div>
-    </v-flex>
-    <v-flex xs12>
-      <div class="text-right">
-        <v-tooltip bottom :disabled="formValid">
-          <template v-slot:activator="{ on }">
-            <span v-on="on">
-              <v-btn :disabled="!formValid" depressed color="primary" type="submit" @click.prevent="sendOrder">Continue</v-btn>
-            </span>
-          </template>
-          <span>Resolve the errors</span>
-        </v-tooltip>
-        <div class="caption text_2--text">You will be sent to {{ selectedProvider }}</div>
-      </div>
-    </v-flex>
-  </v-layout>
+      <v-flex class="mt-10 text-center text_2--text caption">
+        Feel free to
+        <a href="mailto:hello@tor.us?Subject=Topup%20Support%20or%20Inquiry" target="_blank">contact us</a>
+        for support or any inquiry
+      </v-flex>
+    </v-layout>
+  </v-card>
 </template>
 
 <script>
+import config from '../../../config'
 import { paymentProviders, formatCurrencyNumber, significantDigits } from '../../../utils/utils'
 import HelpTooltip from '../../helpers/HelpTooltip'
 
