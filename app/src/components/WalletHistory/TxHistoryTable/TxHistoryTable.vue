ca569d8bfc1a30b774697595cb1bc6da97a49489
Lionell Briones <llenoil@gmail.com>
2019-11-22 10:40:05 +0300

    - Updated pagination - removed commented codes

--- app/src/components/WalletHistory/TxHistoryTable/TxHistoryTable.vue
+++ app/src/components/WalletHistory/TxHistoryTable/TxHistoryTable.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="activity-table" :data-count="transactions.length" :data-per-page="itemsPerPage" :data-count-transfer="nonTopupTransactionCount">
-    <template v-if="filteredTransactions.length > 0">
-      <transaction-details
-        v-for="transaction in filteredTransactions"
-        :key="transaction.id"
-        @onCancelTransaction="cancelTransaction"
-        :transaction="transaction"
-      />
-    </template>
-    <v-layout v-else>
-      <v-flex xs12 class="text-center">No Transaction Activity!</v-flex>
-    </v-layout>
+    <v-data-iterator :items="filteredTransactions" :items-per-page.sync="itemsPerPage" :page.sync="page" hide-default-footer>
+      <template v-slot:default="props">
+        <transaction-details
+          v-for="transaction in props.items"
+          :key="transaction.id"
+          @onCancelTransaction="cancelTransaction"
+          :transaction="transaction"
+        />
+      </template>
+    </v-data-iterator>
 
     <div class="text-center pt-6" v-if="pageCount > 1">
       <v-pagination
@@ -21,78 +20,6 @@
         :length="pageCount"
       ></v-pagination>
     </div>
-    <!-- <v-data-table
-      :headers="headers"
-      :items="filteredTransactions"
-      :expanded.sync="expanded"
-      item-key="id"
-      single-expand
-      @click:row="rowClicked"
-      hide-default-footer
-      :page.sync="page"
-      :items-per-page="itemsPerPage"
-      @page-count="pageCount = $event"
-    >
-      <template v-slot:item.action="{ item }">
-        <span>
-          <v-icon>{{ item.actionIcon }}</v-icon>
-          <span class="transaction-action">{{ item.action }}</span>
-        </span>
-      </template>
-      <template v-slot:item.from="{ item }">
-        <span style="word-break: break-all">{{ item.from }}</span>
-      </template>
-      <template v-slot:item.to="{ item }">
-        <span style="word-break: break-all">{{ item.to }}</span>
-      </template>
-      <template v-slot:item.date="{ item }">
-        <span class="transaction-date">{{ item.dateFormatted }}</span>
-      </template>
-      <template v-slot:item.status="{ item }">
-        <span class="text-capitalize" :class="`text-${item.status.toLowerCase()}`">{{ item.statusText }}</span>
-      </template>
-      <template v-slot:expanded-item="{ headers, item }">
-        <td :colspan="headers.length" class="pa-0 ma-0" style="height: inherit" v-show="item.etherscanLink !== ''">
-          <v-flex xs12 class="pa-4" :class="$vuetify.theme.dark ? 'background lighten-2' : ''">
-            <v-layout wrap>
-              <v-flex xs4 sm1 pr-2>
-                Rate
-                <span class="float-right">:</span>
-              </v-flex>
-              <v-flex xs8 sm11>1 ETH = {{ item.ethRate }} {{ item.currencyUsed }} @ {{ item.timeFormatted }} {{ JSON.stringify(item.gas) }}</v-flex>
-              <v-flex xs4 sm1 pr-2>
-                Network
-                <span class="float-right">:</span>
-              </v-flex>
-              <v-flex xs8 sm11>{{ (mapper[item.networkType] && mapper[item.networkType].networkName) || '' }}</v-flex>
-
-              <v-flex xs12 class="text-right" v-if="Object.keys(item.gas).length">
-                <v-btn color="primary" class="px-6" @click="speedUpTx = true">Speed up transaction</v-btn>
-              </v-flex>
-
-              <v-dialog v-model="speedUpTx" max-width="1000" :fullscreen="$vuetify.breakpoint.xsOnly">
-                <SpeedUpTransaction :gas="item.gas" @onClose="speedUpTx = false" />
-              </v-dialog>
-              <v-flex xs12 class="text-right">
-                <a class="v-btn" color="primary" :href="item.etherscanLink" target="_blank">View On Etherscan</a>
-              </v-flex>
-            </v-layout>
-          </v-flex>
-        </td>
-      </template>
-      <template v-slot:no-data>
-        <v-flex xs12 class="text-center">No Transaction Activity!</v-flex>
-      </template>
-    </v-data-table>
-    <div class="text-center pt-6" v-if="pageCount > 1">
-      <v-pagination
-        class="activity-pagination"
-        prev-icon="$vuetify.icons.page_prev"
-        next-icon="$vuetify.icons.page_next"
-        v-model="page"
-        :length="pageCount"
-      ></v-pagination>
-    </div> -->
   </div>
 </template>
 
@@ -108,7 +35,6 @@ export default {
   data() {
     return {
       page: 1,
-      pageCount: 0,
       itemsPerPage: 8,
       expanded: [],
       pagination: {},
@@ -156,6 +82,9 @@ export default {
   computed: {
     showFooter() {
       return this.transactions && this.transactions.length > 5
+    },
+    pageCount() {
+      return Math.ceil(this.filteredTransactions.length / this.itemsPerPage)
     }
   },
   methods: {
