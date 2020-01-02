4640750338b98e4263b04faf002a184170487c11
Lionell Briones <llenoil@gmail.com>
2020-01-02 10:35:21 +0300

    Import locals from torus backend

--- app/scripts/importLocales.js
+++ app/scripts/importLocales.js
@@ -1,44 +1,28 @@
-const https = require('https')
+const http = require('http')
 var fs = require('fs')
+var log = require('loglevel')
 
-const apiKey = process.env.AIRTABLE_API_KEY || 'keyZ491mmTxAHPvMC'
-const airtableLocaleUrl = 'https://api.airtable.com/v0/appL07XTwLGY3UBvF'
+const localeUrl = 'http://localhost:2020/locales'
 
-const localGroups = [
-  'locales-login',
-  'locales-nav-bar',
-  'locales-account-menu',
-  'locales-wallet-home',
-  'locales-wallet-transfer',
-  'locales-wallet-top-up',
-  'locales-wallet-activity',
-  'locales-wallet-settings'
-]
-let promises = []
-
-for (let i = 0; i < localGroups.length; i++) {
-  promises.push(getLocale(localGroups[i]))
-}
-
-Promise.all(promises).then(function(results) {
-  let locales = []
-  for (let i = 0; i < results.length; i++) {
-    processRecords(results[i], locales)
-  }
-
-  // Create json files
-  for (var localeKey in locales) {
-    if (Object.prototype.hasOwnProperty.call(locales, localeKey)) {
-      fs.writeFile(`src/plugins/i18n/${localeKey}.json`, JSON.stringify(locales[localeKey], null, 2), function(err) {
-        if (err) throw err
-      })
+getLocale()
+  .then(result => {
+    const locales = result.data
+    // Create json files
+    for (var localeKey in locales) {
+      if (Object.prototype.hasOwnProperty.call(locales, localeKey)) {
+        fs.writeFile(`src/plugins/i18n/${localeKey}.json`, JSON.stringify(locales[localeKey], null, 2), function(err) {
+          if (err) throw err
+        })
+      }
     }
-  }
-})
+  })
+  .catch(e => {
+    log.error(e)
+  })
 
-function getLocale(localGroup) {
+function getLocale() {
   return new Promise(function(resolve, reject) {
-    let request = https.get(`${airtableLocaleUrl}/${localGroup}?api_key=${apiKey}`, function(res) {
+    let request = http.get(`${localeUrl}`, function(res) {
       var body = ''
       res.on('data', function(data) {
         body += data
@@ -54,30 +38,4 @@ function getLocale(localGroup) {
 
     request.end()
   })
-}
-
-function processRecords(data, locales) {
-  var records = data.records
-
-  // Map locales
-  records.forEach(({ fields }) => {
-    const name = fields.name
-    delete fields['name']
-    Object.keys(fields).forEach(key => {
-      if (!Object.prototype.hasOwnProperty.call(locales, key)) locales[key] = {}
-      const nameArr = name.split('.')
-      nameKeyMap(locales[key], nameArr, fields[key])
-    })
-  })
-}
-
-function nameKeyMap(mainObject, keyLeft, value) {
-  const key = keyLeft[0]
-  keyLeft.splice(0, 1)
-  if (key !== '' && !Object.prototype.hasOwnProperty.call(mainObject, key)) mainObject[key] = {}
-  if (keyLeft.length === 0) {
-    mainObject[key] = value
-  } else {
-    nameKeyMap(mainObject[key], keyLeft, value)
-  }
 }
