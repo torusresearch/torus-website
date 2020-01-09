d905e9bf718ba6a4990d9726f84e58c0b95cd8c0
iamonuwa <matrix4u2002@gmail.com>
2020-01-09 04:11:14 +0100

    Pass state to redirect

--- app/src/config.js
+++ app/src/config.js
@@ -1,25 +1,10 @@
-import { PromiseReference } from './utils/utils'
 const baseUrl = process.env.VUE_APP_BASE_ROUTE || 'https://localhost:3001'
 
 const baseRoute = baseUrl + process.env.BASE_URL
 
 const redirect_uri = `${baseUrl}/redirect`
 
-export const nodeDetails = {
-  skip: true, // skip fetching of node details and use defaults below
-  updated: new PromiseReference(),
-  minEpoch: 12,
-  currentEpoch: 0,
-  nodeListAddress: '0x97c85658cd10b386fc37e4b32df90b916b689ee8',
-  torusNodeEndpoints: [
-    'https://binance-main-13.torusnode.com/jrpc',
-    'https://waseda-main-13.torusnode.com/jrpc',
-    'https://vgr-main-13.torusnode.com/jrpc',
-    'https://torus-main-13.torusnode.com/jrpc',
-    'https://etc-main-13.torusnode.com/jrpc'
-  ],
-  torusIndexes: [1, 2, 3, 4, 5]
-}
+const verifier_uri = process.env.VERIFIER_URI || 'http://localhost:8080'
 
 export default {
   baseUrl: baseUrl,
@@ -50,5 +35,6 @@ export default {
   DISCORD_CLIENT_ID: '630308572013527060',
   redirect_uri: redirect_uri,
   // api: 'http://localhost:2020'
-  api: 'https://api.tor.us'
+  api: 'https://api.tor.us',
+  verifier_uri
 }
