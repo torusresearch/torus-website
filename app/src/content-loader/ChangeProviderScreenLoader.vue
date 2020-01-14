ccdde1f9049b275f420d700730509713fe79bea4
Lionell Briones <llenoil@gmail.com>
2020-01-14 04:13:31 +0300

    Updated confirm change provider and user info loaders

--- app/src/content-loader/ChangeProviderScreenLoader.vue
+++ app/src/content-loader/ChangeProviderScreenLoader.vue
@@ -1,15 +1,9 @@
 <template>
   <div class="sk-grid">
-    <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 25px; margin-bottom: 2px;"></div></div>
-    <div class="sk-grid__col-mob-2"></div>
-    <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 10px; margin-bottom: 20px;"></div></div>
+    <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 50px; margin-top: 30px; margin-bottom: 20px;"></div></div>
     <div class="sk-grid__col-mob-2"></div>
 
-    <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 15px; margin-bottom: 2;"></div></div>
-    <div class="sk-grid__col-mob-4"><div class="sk-loader" style="height: 50px; margin-bottom: 15px;"></div></div>
-
-    <div class="sk-grid__col-mob-3"><div class="sk-loader" style="height: 15px; margin-bottom: 30px;"></div></div>
-    <div class="sk-grid__col-mob-1"></div>
+    <div class="sk-grid__col-mob-4"><div class="sk-loader" style="height: 100px; margin-bottom: 20px;"></div></div>
 
     <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 50px; margin-bottom: 30px;"></div></div>
     <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 50px; margin-bottom: 30px;"></div></div>
--- app/src/content-loader/PopupScreenLoader.vue
+++ app/src/content-loader/PopupScreenLoader.vue
@@ -1,22 +1,11 @@
 <template>
   <div class="sk-grid">
-    <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 25px; margin-bottom: 2px;"></div></div>
-    <div class="sk-grid__col-mob-2"></div>
-    <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 10px; margin-bottom: 20px;"></div></div>
+    <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 50px; margin-top: 30px; margin-bottom: 20px;"></div></div>
     <div class="sk-grid__col-mob-2"></div>
 
-    <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 15px; margin-bottom: 2;"></div></div>
-    <div class="sk-grid__col-mob-4"><div class="sk-loader" style="height: 50px; margin-bottom: 15px;"></div></div>
+    <div class="sk-grid__col-mob-4"><div class="sk-loader" style="height: 50px; margin-bottom: 20px;"></div></div>
 
-    <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 15px; margin-bottom: 2px;"></div></div>
-    <div class="sk-grid__col-mob-2"></div>
-    <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 50px; margin-bottom: 30px;"></div></div>
-    <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 50px; margin-bottom: 30px;"></div></div>
-
-    <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 15px; margin-bottom: 2;"></div></div>
-    <div class="sk-grid__col-mob-4"><div class="sk-loader" style="height: 50px; margin-bottom: 15px;"></div></div>
-
-    <div class="sk-grid__col-mob-2 sk-grid__col-mob-shift-2"><div class="sk-loader" style="height: 15px; margin-bottom: 30px;"></div></div>
+    <div class="sk-grid__col-mob-4"><div class="sk-loader" style="height: 80px; margin-bottom: 20px;"></div></div>
 
     <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 50px; margin-bottom: 30px;"></div></div>
     <div class="sk-grid__col-mob-2"><div class="sk-loader" style="height: 50px; margin-bottom: 30px;"></div></div>
