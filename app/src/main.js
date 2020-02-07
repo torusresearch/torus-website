0b9e92f8dc4cf3a651bc95ac5cd796d41c8c6d0d
Leonard Tan <tetratorus@gmail.com>
2020-02-07 17:23:19 +0800

    Add alpha3 testnet configs

--- app/src/main.js
+++ app/src/main.js
@@ -19,7 +19,7 @@ switch (buildEnv) {
     logLevel = 'info'
     log.setDefaultLevel(logLevel)
     break
-  case 'testing' || 'development' || 'lrc' || 'alpha':
+  case 'testing' || 'development' || 'lrc' || 'alpha3':
     logLevel = 'debug'
     log.setDefaultLevel(logLevel)
     break
