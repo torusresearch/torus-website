0a2c1c8fe5ca1a58d3a97c07fad2475cd28e6f32
Leonard Tan <tetratorus@gmail.com>
2020-02-07 17:12:27 +0800

    Add alpha1 testnet configs

--- app/src/registerServiceWorker.js
+++ app/src/registerServiceWorker.js
@@ -14,7 +14,7 @@ if (
     process.env.VUE_APP_TORUS_BUILD_ENV === 'staging' ||
     process.env.VUE_APP_TORUS_BUILD_ENV === 'testing' ||
     process.env.VUE_APP_TORUS_BUILD_ENV === 'lrc' ||
-    process.env.VUE_APP_TORUS_BUILD_ENV === 'alpha')
+    process.env.VUE_APP_TORUS_BUILD_ENV === 'alpha1')
 ) {
   // if swIntegrity is not calculated
   if (swIntegrity === ['SERVICE', 'WORKER', 'SHA', 'INTEGRITY'].join('_')) {
