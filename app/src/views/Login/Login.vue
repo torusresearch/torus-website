6ac3a6cc9d377b848bcbe773d29379b317f95be4
iamonuwa <matrix4u2002@gmail.com>
2020-02-12 21:59:32 +0100

    show torus oauth only on lrc

--- app/src/views/Login/Login.vue
+++ app/src/views/Login/Login.vue
@@ -93,7 +93,7 @@
                 </v-flex>
               </v-layout>
             </v-flex>
-            <v-flex xs9 sm7 ml-auto mb-2 mr-auto>
+            <v-flex v-show="canShowLrc" xs9 sm7 ml-auto mb-2 mr-auto>
               <v-btn
                 id="torusLogin"
                 :large="!$vuetify.breakpoint.xsOnly"
@@ -110,7 +110,7 @@
                 Sign up/in with Email
               </v-btn>
             </v-flex>
-            <v-flex xs9 sm7 ml-auto mb-2 mr-auto>
+            <v-flex v-show="canShowLrc" xs9 sm7 ml-auto mb-2 mr-auto>
               <v-btn
                 id="torusLogin"
                 :large="!$vuetify.breakpoint.xsOnly"
@@ -259,6 +259,9 @@ export default {
       } else {
         return this.$vuetify.breakpoint.xsOnly ? WalletHomeLoaderMobile : WalletHomeLoader
       }
+    },
+    canShowLrc() {
+      return process.env.VUE_APP_TORUS_BUILD_ENV === 'lrc'
     }
   }),
   watch: {
