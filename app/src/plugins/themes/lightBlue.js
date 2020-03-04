370ac2156ef325fb28ecef5905bc16beb4c37a37
Lionell Briones <llenoil@gmail.com>
2020-03-04 20:30:58 +0800

    Initialize v8

--- app/src/plugins/themes/lightBlue.js
+++ app/src/plugins/themes/lightBlue.js
@@ -1,4 +1,19 @@
 export default {
+  // V8
+  brand_1: '#0364FF',
+  brand_2: '#5495F7',
+  brand_3: '#CDE0FF',
+  brand_hover: '#075DE5',
+  gray_1: '#D3D5E2',
+  gray_2: '#EEF2F4',
+  gray_3: '#A2A5B5',
+  gray_4: '#F9F9FB',
+  font_1: '#5C6C7F',
+  font_2: '#7F8FA4',
+  font_link_1: '#0364FF',
+  font_link_2: '#5495F7',
+  torus_black: '#252529',
+
   // Main background color, supports 2 color gradient
   background_body_1: '#FCFCFC',
   background_body_2: '#FCFCFC',
