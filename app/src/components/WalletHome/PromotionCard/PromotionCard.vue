c46a54c6f16926cc8db6df40cde4d0f21b2492f6
Lionell Briones <llenoil@gmail.com>
2020-03-05 10:06:31 +0800

    Update home UI

--- app/src/components/WalletHome/PromotionCard/PromotionCard.vue
+++ app/src/components/WalletHome/PromotionCard/PromotionCard.vue
@@ -1,15 +1,16 @@
 <template>
-  <v-card class="card-shadow">
+  <v-card class="elevation-1 promotion-card">
     <v-card-text class="pt-0 px-6" :class="$vuetify.breakpoint.lgAndUp ? 'pb-2' : $vuetify.breakpoint.xsOnly ? 'pb-6' : 'pb-3'">
       <v-layout>
         <v-flex class="text_1--text pt-6" :class="$vuetify.breakpoint.xsOnly ? 'text-center xs12' : $vuetify.breakpoint.lgAndUp ? 'xs8' : 'xs9'">
-          <div class="body-1 font-weight-bold" :class="subtitle ? 'text-clamp-one' : 'text-clamp-two'">{{ title }}</div>
+          <div class="title font-weight-bold" :class="subtitle ? 'text-clamp-one' : 'text-clamp-two'">{{ title }}</div>
           <slot name="subtitle">
-            <div class="text-clamp-one" :class="$vuetify.breakpoint.lgAndUp ? 'body-2' : 'caption'">{{ subtitle }}</div>
+            <div class="text-clamp-one caption mt-2">{{ subtitle }}</div>
             <div class="more-details-container">
               <v-btn
                 color="primary"
                 depressed
+                large
                 class="py-1 mt-4 white--text"
                 :class="$vuetify.breakpoint.smAndDown ? 'px-8' : 'px-12'"
                 :href="detailsLink"
@@ -22,7 +23,7 @@
         </v-flex>
         <slot name="image">
           <v-flex xs4 pt-4 class="text-right hidden-xs-only">
-            <img :src="imagePath" :style="$vuetify.breakpoint.smAndDown ? 'width: 100%' : 'width: 100px'" :alt="`${title} Image`" />
+            <img :src="imagePath" :style="$vuetify.breakpoint.smAndDown ? 'width: 100%' : 'height: 92px'" :alt="`${title} Image`" />
           </v-flex>
         </slot>
       </v-layout>
