286dd6b3b84425a331d49c988dbc6b03d8cc1159
Lionell Briones <llenoil@gmail.com>
2020-03-23 09:07:10 +0800

    UI Fixes

--- app/src/utils/utils.js
+++ app/src/utils/utils.js
@@ -481,3 +481,7 @@ export const fakeStream = {
 export function formatSmallNumbers(number, currency = 'usd') {
   return `${currency.toLowerCase() === 'usd' ? significantDigits(number, 5).toFixed(2) : significantDigits(number, 2).toFixed(5)} ${currency}`
 }
+
+export function formatSmallNumbers(number, currency = 'usd') {
+  return `${currency.toLowerCase() === 'usd' ? significantDigits(number, 5) : significantDigits(number, 2)} ${currency}`
+}
