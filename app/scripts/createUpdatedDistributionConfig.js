cbc7b216fe16a41ee0845c87613f7cc9e06c128d
chaitanyapotti <chaitanya.potti@gmail.com>
2020-04-03 15:30:44 +0800

    Working code

--- app/scripts/createUpdatedDistributionConfig.js
+++ app/scripts/createUpdatedDistributionConfig.js
@@ -9,7 +9,7 @@ try {
     if (newConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items.length > 0) {
       const requiredAssociation = newConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items.find((x) => x.EventType === 'origin-request')
       let arn = requiredAssociation.LambdaFunctionARN
-      const newVersion = parseInt(arn.slice(-1), 10) + 1
+      const newVersion = Number.parseInt(arn.slice(-1), 10) + 1
       arn = arn.slice(0, -1) + newVersion
       requiredAssociation.LambdaFunctionARN = arn
     }
