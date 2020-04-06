7c0f3f601db1a8202672edfb9b976a67fe649026
Lionell Briones <llenoil@gmail.com>
2020-04-06 08:52:17 +0800

    Updated login page

--- app/src/views/Login/Login.vue
+++ app/src/views/Login/Login.vue
@@ -1,100 +1,89 @@
 <template>
   <div :class="[{ 'background-login': !loggedIn }, 'default']">
     <template v-if="!loginInProgress">
-      <v-layout wrap fill-height align-center justify-center class="login-panel-left">
+      <v-layout wrap fill-height align-center justify-center class="login-panel-left" :class="$vuetify.theme.dark ? 'torus-dark' : ''">
         <v-flex xs12 sm8 md6>
           <v-layout v-if="!isLogout" wrap>
-            <v-flex class="mb-5" xs9 sm7 ml-auto mr-auto>
+            <v-flex class="mb-5" xs10 sm8 ml-auto mr-auto>
               <img width="180" :src="require(`../../../public/images/torus-logo-${$vuetify.theme.dark ? 'white' : 'blue'}.svg`)" />
             </v-flex>
-            <v-flex class="mb-2" xs9 sm7 ml-auto mr-auto>
-              <span class="display-1 font-weight-bold">{{ t('login.welcome') }} Torus</span>
+            <v-flex class="mb-2" xs10 sm8 ml-auto mr-auto>
+              <div
+                class="verifier-title font-weight-bold"
+                :class="[$vuetify.theme.dark ? '' : 'text_2--text', $vuetify.breakpoint.xsOnly ? 'display-1' : 'display-2']"
+              >
+                <span>
+                  Your
+                  <span v-if="activeButton === GOOGLE">
+                    <span class="verifier-title__google-blue">G</span>
+                    <span class="verifier-title__google-red">o</span>
+                    <span class="verifier-title__google-yellow">o</span>
+                    <span class="verifier-title__google-blue">g</span>
+                    <span class="verifier-title__google-green">l</span>
+                    <span class="verifier-title__google-red">e</span>
+                  </span>
+                  <span v-else-if="activeButton === FACEBOOK" class="verifier-title__facebook">Facebook</span>
+                  <span v-else-if="activeButton === REDDIT" class="verifier-title__reddit">Reddit</span>
+                  <span v-else-if="activeButton === TWITCH" class="verifier-title__twitch">Twitch</span>
+                  <span v-else-if="activeButton === DISCORD" class="verifier-title__discord">Discord</span>
+                </span>
+              </div>
+              <div
+                class="font-weight-bold"
+                :class="[$vuetify.theme.dark ? '' : 'text_2--text', $vuetify.breakpoint.xsOnly ? 'headline' : 'display-2']"
+              >
+                digital wallet in one-click
+              </div>
             </v-flex>
-            <v-flex class="body-2" mb-8 xs9 sm7 ml-auto mr-auto>
-              <span>{{ t('login.message') }}</span>
+            <v-flex xs10 sm8 ml-auto mr-auto :class="[$vuetify.breakpoint.xsOnly ? 'mt-8' : 'mt-11']">
+              <div class="headline font-weight-light" :class="$vuetify.theme.dark ? '' : 'text_2--text'">Sign up/in with</div>
             </v-flex>
-            <v-flex xs9 sm7 ml-auto mb-2 mr-auto>
+            <v-flex v-if="$vuetify.breakpoint.xsOnly" xs10 sm8 ml-auto mt-2 mr-auto>
               <v-btn
                 id="loginBtn"
                 :color="$vuetify.theme.dark ? '' : 'white'"
                 block
                 :class="$vuetify.theme.dark ? 'torus-dark' : ''"
-                class="body-1 font-weight-bold card-shadow-v8 text_2--text login-btn"
+                class="body-1 font-weight-bold card-shadow-v8 text_2--text login-btn-google"
                 @click="triggerLogin({ verifier: GOOGLE, calledFromEmbed: false })"
               >
-                <img
-                  :src="require(`../../../public/img/icons/google${$vuetify.theme.dark ? '-dark' : ''}.svg`)"
-                  :class="$vuetify.theme.dark ? 'mr-4 torus-dark' : 'mr-6'"
-                />
+                <img class="mr-5" :src="require(`../../../public/img/icons/login-google.svg`)" :class="$vuetify.theme.dark ? 'torus-dark' : ''" />
                 {{ t('login.signIn') }} Google
               </v-btn>
             </v-flex>
-            <v-flex xs9 sm7 ml-auto mr-auto mb-3>
-              <v-layout wrap class="other-login-container">
-                <v-flex xs6 px-1>
+            <v-flex v-if="$vuetify.breakpoint.xsOnly" xs10 sm8 ml-auto mr-auto>
+              <v-layout wrap mx-n1>
+                <v-flex v-for="verifier in loginButtonsMobile" :key="verifier" xs4 px-1 mt-2>
                   <v-btn
-                    :large="!$vuetify.breakpoint.xsOnly"
-                    :color="$vuetify.theme.dark ? '' : 'white'"
-                    :class="$vuetify.theme.dark ? 'torus-dark' : 'card-shadow-v8'"
-                    :depressed="$vuetify.theme.dark"
-                    block
-                    class="other-login-btn"
+                    class="login-btn login-btn--mobile"
+                    :class="{ active: verifier === activeButton }"
                     type="button"
-                    :title="`${t('login.loginWith')} Facebook`"
-                    @click="triggerLogin({ verifier: FACEBOOK, calledFromEmbed: false })"
+                    :title="`${t('login.loginWith')} ${verifier}`"
+                    @click="triggerLogin({ verifier: verifier, calledFromEmbed: false })"
+                    @mouseover="activeButton = verifier"
                   >
-                    <img height="32" :src="require(`../../../public/img/icons/facebook${$vuetify.theme.dark ? '-dark' : ''}.svg`)" />
-                  </v-btn>
-                </v-flex>
-                <v-flex xs6 px-1>
-                  <v-btn
-                    :large="!$vuetify.breakpoint.xsOnly"
-                    :color="$vuetify.theme.dark ? '' : 'white'"
-                    :class="$vuetify.theme.dark ? 'torus-dark' : 'card-shadow-v8'"
-                    :depressed="$vuetify.theme.dark"
-                    block
-                    class="other-login-btn"
-                    type="button"
-                    :title="`${t('login.loginWith')} Reddit`"
-                    @click="triggerLogin({ verifier: REDDIT, calledFromEmbed: false })"
-                  >
-                    <img height="32" :src="require(`../../../public/img/icons/reddit${$vuetify.theme.dark ? '-dark' : ''}.svg`)" />
-                  </v-btn>
-                </v-flex>
-                <v-flex xs6 px-1>
-                  <v-btn
-                    :large="!$vuetify.breakpoint.xsOnly"
-                    :color="$vuetify.theme.dark ? '' : 'white'"
-                    :class="$vuetify.theme.dark ? 'torus-dark' : 'card-shadow-v8'"
-                    :depressed="$vuetify.theme.dark"
-                    block
-                    class="other-login-btn"
-                    type="button"
-                    :title="`${t('login.loginWith')} Twitch`"
-                    @click="triggerLogin({ verifier: TWITCH, calledFromEmbed: false })"
-                  >
-                    <img height="32" :src="require(`../../../public/img/icons/twitch${$vuetify.theme.dark ? '-dark' : ''}.svg`)" />
-                  </v-btn>
-                </v-flex>
-                <v-flex xs6 px-1>
-                  <v-btn
-                    :large="!$vuetify.breakpoint.xsOnly"
-                    :color="$vuetify.theme.dark ? '' : 'white'"
-                    :class="$vuetify.theme.dark ? 'torus-dark' : 'card-shadow-v8'"
-                    :depressed="$vuetify.theme.dark"
-                    block
-                    class="other-login-btn"
-                    type="button"
-                    :title="`${t('login.loginWith')} Discord`"
-                    @click="triggerLogin({ verifier: DISCORD, calledFromEmbed: false })"
-                  >
-                    <img height="32" :src="require(`../../../public/img/icons/discord${$vuetify.theme.dark ? '-dark' : ''}.svg`)" />
+                    <img :src="require(`../../../public/img/icons/login-${verifier}.svg`)" />
                   </v-btn>
                 </v-flex>
               </v-layout>
             </v-flex>
-            <v-flex class="caption" mb-6 xs9 sm7 ml-auto mr-auto>
-              <span class="text_2--text body-1">
+            <v-flex v-if="!$vuetify.breakpoint.xsOnly" xs10 sm8 ml-auto mr-auto mt-4>
+              <v-btn
+                v-for="verifier in loginButtons"
+                :key="verifier"
+                class="login-btn"
+                :class="{ active: verifier === activeButton }"
+                type="button"
+                :title="`${t('login.loginWith')} ${verifier}`"
+                @click="triggerLogin({ verifier: verifier, calledFromEmbed: false })"
+                @mouseover="activeButton = verifier"
+              >
+                <img v-if="verifier === activeButton" :src="require(`../../../public/img/icons/login-${verifier}.svg`)" />
+                <img v-else :src="require(`../../../public/img/icons/login-${verifier}-${$vuetify.theme.dark ? 'white' : 'grey'}.svg`)" />
+              </v-btn>
+            </v-flex>
+            <v-flex mb-6 xs10 sm8 ml-auto mr-auto mt-12>
+              <span class="body-1" :class="$vuetify.theme.dark ? '' : 'text_2--text'">
                 {{ t('login.acceptTerms') }}
                 <a href="https://docs.tor.us/legal/terms-and-conditions" target="_blank">
                   <span class="torusBrand1--text">{{ t('login.termsAndConditions') }}</span>
@@ -128,21 +117,21 @@
             </v-flex>
           </v-layout>
         </v-flex>
-        <v-flex
-          v-if="$vuetify.breakpoint.smAndUp"
-          xs12
-          sm4
-          md6
-          fill-height
-          class="login-panel-right"
-          :class="$vuetify.theme.dark ? 'torus-dark' : ''"
-        >
-          <v-layout class="pb-8" wrap fill-height align-end>
-            <v-flex class="mb-3 text-center" xs9 sm8 md10 ml-auto mr-auto>
-              <div class="right-panel-header white--text font-weight-bold mb-2">{{ t('login.frictionless') }}</div>
-              <div class="body-2 right-panel-subheader white--text mx-auto">
-                {{ t('login.simpleSecure') }}
-              </div>
+        <v-flex v-if="$vuetify.breakpoint.smAndUp" sm4 md6 fill-height class="login-panel-right" :class="$vuetify.theme.dark ? 'torus-dark' : ''">
+          <v-layout wrap fill-height align-center>
+            <v-flex xs12 text-center>
+              <v-carousel cycle height="650" interval="4000" :show-arrows="false" hide-delimiters>
+                <v-carousel-item v-for="slide in slides" :key="slide.id">
+                  <img
+                    class="mb-7 login-panel-right__image"
+                    :src="require(`../../../public/images/login-bg-${$vuetify.theme.dark ? 'dark-' : ''}${slide.id}.png`)"
+                  />
+                  <div class="display-1 mb-3" :class="$vuetify.theme.dark ? '' : 'text_2--text'">{{ slide.title }}</div>
+                  <div class="body-1" :class="$vuetify.theme.dark ? '' : 'text_2--text'">{{ slide.sub_title1 }}</div>
+                  <div class="body-1" :class="$vuetify.theme.dark ? '' : 'text_2--text'">{{ slide.sub_title2 }}</div>
+                  <v-btn class="learn-more-btn" :class="{ isDark: $vuetify.theme.dark }" :href="slide.link" target="_blank">Learn More</v-btn>
+                </v-carousel-item>
+              </v-carousel>
             </v-flex>
           </v-layout>
         </v-flex>
@@ -181,11 +170,37 @@ export default {
   data() {
     return {
       isLogout: false,
-      FACEBOOK,
       GOOGLE,
-      TWITCH,
+      FACEBOOK,
       REDDIT,
+      TWITCH,
       DISCORD,
+      loginButtons: [GOOGLE, FACEBOOK, REDDIT, TWITCH, DISCORD],
+      loginButtonsMobile: [FACEBOOK, REDDIT, TWITCH, DISCORD],
+      activeButton: GOOGLE,
+      slides: [
+        {
+          id: 1,
+          title: 'Send and receive digital currencies via email',
+          sub_title1: 'Transacting on blockchain has never been easier.',
+          sub_title2: 'An email is all you need to get started.',
+          link: 'https://www.google.com',
+        },
+        {
+          id: 2,
+          title: 'Purchase digital currencies globally with credit card',
+          sub_title1: 'Choose from a range providers',
+          sub_title2: 'Get currencies at one of the most competitive rates',
+          link: 'https://www.google.com',
+        },
+        {
+          id: 3,
+          title: 'Interact with thousands of apps on the blockchain',
+          sub_title1: 'From Finance, Games, Exchanges and more',
+          sub_title2: 'Access the decentralised world with Torus',
+          link: 'https://www.google.com',
+        },
+      ],
     }
   },
   computed: mapState({
