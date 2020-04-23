de7759b8fb6316e08defe4dcb72243a673ec9895
Lionell Briones <llenoil@gmail.com>
2020-04-23 10:56:00 +0800

    enable gtm for alpha 4

--- app/src/main.js
+++ app/src/main.js
@@ -39,8 +39,8 @@ log.info('VUE_APP_TORUS_BUILD_ENV', process.env.VUE_APP_TORUS_BUILD_ENV)
 
 Vue.use(VueGtm, {
   id: 'GTM-PDF8MFV', // Your GTM single container ID or array of container ids ['GTM-xxxxxxx', 'GTM-yyyyyyy']
-  enabled: buildEnvironment === 'production', // defaults to true.
-  debug: false, // Whether or not display console logs debugs (optional)
+  enabled: true, // defaults to true.
+  debug: true, // Whether or not display console logs debugs (optional)
   loadScript: true,
   vueRouter: router, // Pass the router instance to automatically sync with router (optional)
 })
