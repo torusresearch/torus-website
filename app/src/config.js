c1af6b9a56cfb13b6512d745f9601b8e2fb2c655
Lionell Briones <llenoil@gmail.com>
2020-09-14 14:54:01 +0800

    Update wechat name

--- app/src/config.js
+++ app/src/config.js
@@ -232,7 +232,7 @@ export default {
     [WECHAT_VERIFIER]: {
       description: '',
       typeOfLogin: WECHAT,
-      name: 'Wechat',
+      name: 'WeChat',
       clientId: VUE_APP_WECHAT_CLIENT_ID,
       logoHover: '',
       logoLight: '',
