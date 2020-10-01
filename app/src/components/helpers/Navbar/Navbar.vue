c10c05cb520773e666a3250f93fb3f8f81592d3c
Lionell Briones <llenoil@gmail.com>
2020-10-01 14:21:51 +0800

    Fix navbar logo height

--- app/src/components/helpers/Navbar/Navbar.vue
+++ app/src/components/helpers/Navbar/Navbar.vue
@@ -6,20 +6,14 @@
         class="home-link mr-1"
         alt="Torus Logo"
         :width="$store.state.whiteLabel.isActive ? '' : '135'"
-        :height="$store.state.whiteLabel.isActive ? '30' : '30'"
+        height="30"
         :src="getLogo.logo"
       />
       <img v-else src="../../../assets/img/icons/t-fill.svg" width="35" height="30" alt="Torus Logo" />
     </div>
     <div v-else class="d-flex align-end">
       <router-link v-if="!$vuetify.breakpoint.xsOnly" :to="{ name: 'walletHome' }">
-        <img
-          class="home-link mr-1"
-          alt="Torus Logo"
-          :width="$store.state.whiteLabel.isActive ? '' : '135'"
-          :height="$store.state.whiteLabel.isActive ? '30' : '30'"
-          :src="getLogo.logo"
-        />
+        <img class="home-link mr-1" alt="Torus Logo" :width="$store.state.whiteLabel.isActive ? '' : '135'" height="30" :src="getLogo.logo" />
       </router-link>
       <router-link v-if="$vuetify.breakpoint.xsOnly" id="logo-home-lnk" :to="{ name: 'walletHome' }">
         <img src="../../../assets/img/icons/t-fill.svg" width="35" height="30" alt="Torus Logo" />
