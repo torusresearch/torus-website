d6a514238127affaa1bc810dd0db6fff00afa8d7
Leonard Tan <tetratorus@gmail.com>
2020-11-02 17:20:47 +0800

    Add styling to index html for popup

--- app/extensionPlugin.js
+++ app/extensionPlugin.js
@@ -1,3 +1,4 @@
+const { parse } = require('node-html-parser')
 /* eslint-disable */
 module.exports = class ExtensionPlugin {
   constructor() {}
@@ -5,6 +6,7 @@ module.exports = class ExtensionPlugin {
   apply(compiler) {
     const ID = 'vue-extension'
     const manifestName = 'manifest.json'
+    const popupPageName = 'index.html'
     compiler.hooks.emit.tap(ID, (compilation) => {
       try {
         // const manifest = JSON.parse(compilation.assets[manifestName].source())
@@ -18,11 +20,26 @@ module.exports = class ExtensionPlugin {
           default_src: 'index.html',
         }
         manifest.permissions = ['activeTab', 'storage']
-        manifest.content_security_policy = `script-src 'self' 'unsafe-eval'; object-src 'self';`
+
+        // inline scripts in Chrome extensions need SRI in the CSP
+        const inlineScriptSHA256Integrities = `'sha256-O1sOOlFv4s/n2Ua1NIIC5/IpFKNPfBU53voHvgjtAJ0=' 'sha256-iPUHWPCQMD2MARNLfjdsfC9KdbMv98FCVtbaZRs0gSs='`
+
+        manifest.content_security_policy = `script-src 'self' ${inlineScriptSHA256Integrities} 'unsafe-eval'; object-src 'self';`
         const newManifestSource = JSON.stringify(manifest)
         compilation.assets[manifestName] = {
           source: () => newManifestSource,
           size: () => newManifestSource.length,
+        }
+        const popupPage = compilation.assets[popupPageName]
+        if (popupPage) {
+          const popupPageSource = popupPage.source()
+          const root = parse(popupPageSource)
+          root.querySelector('html').setAttribute('style', 'width: 500px;')
+          const rootSource = root.toString()
+          compilation.assets[popupPageName] = {
+            source: () => rootSource,
+            size: () => rootSource.length,
+          }
         }
       } catch (error) {
         console.error('Could not run service worker integrity plugin', error)
