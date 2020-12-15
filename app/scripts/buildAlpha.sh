51d338fea96d8b507f92ab712755453617f51e23
chaitanyapotti <chaitanya.potti@gmail.com>
2020-12-15 13:29:48 +0800

    use correct build mode for lrc

--- app/scripts/buildAlpha.sh
+++ app/scripts/buildAlpha.sh
@@ -1,5 +1,13 @@
 export VUE_APP_TORUS_BUILD_ENV="$CIRCLE_BRANCH"
 export VUE_APP_BASE_ROUTE="https://"$CIRCLE_BRANCH".tor.us"
+if [ "$CIRCLE_BRANCH" = "lrc" ]
+then
+    export BUILD_MODE="$CIRCLE_BRANCH"
+else
+    export BUILD_MODE="baselrc"
+fi
+
 echo $VUE_APP_TORUS_BUILD_ENV
 echo $VUE_APP_BASE_ROUTE
-cross-env VUE_APP_TORUS_BUILD_ENV="$CIRCLE_BRANCH" npm run pre-build && vue-cli-service build --mode baselrc
+echo $BUILD_MODE
+cross-env VUE_APP_TORUS_BUILD_ENV="$CIRCLE_BRANCH" npm run pre-build && vue-cli-service build --mode "$BUILD_MODE"
