4cc98c5ab15b9a539315597971a192bdcc5b3aa9
chaitanyapotti <chaitanya.potti@gmail.com>
2020-12-17 17:15:30 +0800

    use global

--- scripts/gitPush.sh
+++ scripts/gitPush.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-git config user.email "chaitanya.potti@gmail.com"
-git config user.name "chaitanyapotti"
+git config --global user.email "chaitanya.potti@gmail.com"
+git config --global user.name "chaitanyapotti"
 if ! (git diff --quiet && git diff --staged --quiet && git diff origin/$CIRCLE_BRANCH HEAD --quiet); then
     git push origin $CIRCLE_BRANCH
     if [[ "$CIRLCE_BRANCH" = 'master' ]]; then
