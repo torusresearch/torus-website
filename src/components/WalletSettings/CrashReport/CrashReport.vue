d3fa7e671a8426e2d1113e1d898c2726994e7706
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-18 15:20:18 +0700

    Use mapState and mapActions in CrashReport

--- src/components/WalletSettings/CrashReport/CrashReport.vue
+++ src/components/WalletSettings/CrashReport/CrashReport.vue
@@ -2,23 +2,23 @@
   <div class="select-theme-container" :class="$vuetify.breakpoint.xsOnly ? 'pt-5' : 'py-5 px-4'">
     <div class="body-2 torusFont1--text mb-2 px-1">{{ t('walletSettings.explainCrashReport') }}</div>
     <v-layout wrap mx-n3>
-      <v-switch v-model="enabled" :label="t(enabled ? 'walletSettings.crashReportIsEnabled' : 'walletSettings.crashReportIsDisabled')"></v-switch>
+      <v-switch
+        :input-value="enabled"
+        :label="t(enabled ? 'walletSettings.crashReportIsEnabled' : 'walletSettings.crashReportIsDisabled')"
+        @change="setCrashReport"
+      ></v-switch>
     </v-layout>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex'
+
 export default {
   name: 'CrashReportSettings',
-  computed: {
-    enabled: {
-      get() {
-        return this.$store.state.crashReport
-      },
-      set(value) {
-        this.$store.dispatch('setCrashReport', Boolean(value))
-      },
-    },
-  },
+  computed: mapState({
+    enabled: 'crashReport',
+  }),
+  methods: mapActions(['setCrashReport']),
 }
 </script>
