5410e89826d5807643f8501c16f325730916c900
Lionell Briones <llenoil@gmail.com>
2021-01-28 13:11:03 +0800

    - added form validation

--- src/components/WalletSettings/TorusKeyDialog/TorusKeyDialog.vue
+++ src/components/WalletSettings/TorusKeyDialog/TorusKeyDialog.vue
@@ -6,21 +6,42 @@
           <div class="font-weight-bold headline">{{ t('walletSettings.customKey.setCustomPrivateKey') }}</div>
         </v-flex>
         <v-flex xs12 mb-4 :class="$vuetify.breakpoint.xsOnly ? '' : 'px-4'">
-          <v-layout wrap align-center>
-            <v-flex>
-              <v-text-field v-model="customPrivateKey" :rules="rules"></v-text-field>
-            </v-flex>
-            <v-flex xs4 sm2 class="ml-auto" px-4>
-              <v-btn block @click="setKey(customPrivateKey)">{{ t('walletSettings.customKey.replace') }}</v-btn>
-            </v-flex>
-          </v-layout>
+          <v-form ref="customPrivateKeyForm" v-model="customPrivateKeyFormValid" lazy-validation @submit.prevent="setKey">
+            <v-layout wrap align-center>
+              <v-flex>
+                <v-text-field v-model="customPrivateKey" :rules="rules.privateKey"></v-text-field>
+              </v-flex>
+              <v-flex xs4 sm2 class="ml-auto" px-4>
+                <v-btn
+                  large
+                  block
+                  class="torus-btn1"
+                  :class="$store.state.whiteLabel.isActive ? 'white--text' : 'torusBrand1--text'"
+                  :color="$store.state.whiteLabel.isActive ? 'torusBrand1' : ''"
+                  type="submit"
+                  :disabled="!customPrivateKeyFormValid"
+                >
+                  {{ t('walletSettings.customKey.replace') }}
+                </v-btn>
+              </v-flex>
+            </v-layout>
+          </v-form>
         </v-flex>
         <v-flex xs12>
           <div class="caption text_3--text mb-4 px-5">{{ t('walletSettings.customKey.noteReplaceWarning') }}</div>
         </v-flex>
         <v-flex xs12>
           <v-flex xs4 sm4 class="mr-auto" px-4>
-            <v-btn block @click="resetKey">{{ t('walletSettings.customKey.resetToDefault') }}</v-btn>
+            <v-btn
+              block
+              large
+              class="torus-btn1"
+              :class="$store.state.whiteLabel.isActive ? 'white--text' : 'torusBrand1--text'"
+              :color="$store.state.whiteLabel.isActive ? 'torusBrand1' : ''"
+              @click="resetKey"
+            >
+              {{ t('walletSettings.customKey.resetToDefault') }}
+            </v-btn>
           </v-flex>
         </v-flex>
       </v-layout>
@@ -31,30 +52,34 @@
     </v-card-text>
     <v-dialog v-model="confirm" max-width="290">
       <v-card>
-        <v-card-title class="headline">
-          {{ t('walletSettings.customKey.areYouSure') }}
-        </v-card-title>
-        <v-card-text>
-          {{
-            nextAction === 'resetKey'
-              ? `${t('walletSettings.customKey.replaceKeyWarning')} ${t('walletSettings.customKey.reloginWarning')}`
-              : `${t('walletSettings.customKey.resetKeyWarning')} ${t('walletSettings.customKey.reloginWarning')}`
-          }}
-        </v-card-text>
-        <v-card-text xs12>{{ 'Please enter the text below to continue.' }}</v-card-text>
-        <v-card-text xs12>
-          <v-text-field
-            v-model="warningCheckText"
-            class="warning-check"
-            placeholder="I agree to deleting my current private key"
-            @keypress="warningCheck"
-          ></v-text-field>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn :disabled="!warningCheckPassed ? true : false" text @click="disagree">{{ t('walletSettings.customKey.disagree') }}</v-btn>
-          <v-btn :disabled="!warningCheckPassed ? true : false" text @click="agree">{{ t('walletSettings.customKey.agree') }}</v-btn>
-        </v-card-actions>
+        <v-form ref="warningForm" v-model="warningFormValid" lazy-validation @submit.prevent="agree">
+          <v-card-title class="headline">
+            {{ t('walletSettings.customKey.areYouSure') }}
+          </v-card-title>
+          <v-card-text>
+            {{
+              nextAction === 'resetKey'
+                ? `${t('walletSettings.customKey.replaceKeyWarning')} ${t('walletSettings.customKey.reloginWarning')}`
+                : `${t('walletSettings.customKey.resetKeyWarning')} ${t('walletSettings.customKey.reloginWarning')}`
+            }}
+          </v-card-text>
+          <v-card-text xs12>{{ t('walletSettings.customKey.enterTextToContinue') }}</v-card-text>
+          <v-card-text xs12>
+            <v-text-field
+              v-model="warningCheckText"
+              class="warning-check"
+              placeholder="I agree to deleting my current private key"
+              :rules="rules.warningText"
+            ></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text type="button" @click="disagree">
+              {{ t('walletSettings.customKey.disagree') }}
+            </v-btn>
+            <v-btn :disabled="!warningFormValid" text type="submit">{{ t('walletSettings.customKey.agree') }}</v-btn>
+          </v-card-actions>
+        </v-form>
       </v-card>
     </v-dialog>
   </v-card>
@@ -79,11 +104,14 @@ export default {
       customPrivateKey: '',
       consent: false,
       warningCheckText: '',
-      warningCheckPassed: false,
-      privateKeyValidated: false,
       nextAction: '',
       nextParams: [],
-      rules: [(v) => this.privateKeyValidation(v)],
+      customPrivateKeyFormValid: false,
+      warningFormValid: false,
+      rules: {
+        privateKey: [(v) => this.privateKeyValidation(v)],
+        warningText: [(v) => this.warningCheck(v)],
+      },
     }
   },
   methods: {
@@ -97,36 +125,28 @@ export default {
         }
         const customPrivBN = new BN(v, 16)
         if (customPrivBN.cmp(new BN(0)) === 0) {
-          this.privateKeyValidated = false
           return this.t('walletSettings.customKey.privateKeyCannotBeZero') // 'Private key cannot be 0'
         }
         const prefixed = addHexPrefix(v)
         const buffer = toBuffer(prefixed)
         if (!isValidPrivate(buffer)) {
-          this.privateKeyValidated = false
           return this.t('walletSettings.customKey.invalidPrivateKey') // 'Invalid private key'
         }
       } catch (error) {
-        this.privateKeyValidated = false
         log.error(error)
         return this.t('walletSettings.customKey.unableToValidatePrivateKey') // 'Unable to validate private key'
       }
-      this.privateKeyValidated = true
       return true
     },
-    warningCheck() {
-      if (this.warningCheckText === 'I agree to deleting my current private key') {
-        this.warningCheckPassed = true
-      } else {
-        this.warningCheckPassed = false
-      }
-      window.console.log(this.warningCheckText, this.warningCheckPassed, 'I agree to deleting my current private key')
+    warningCheck(text) {
+      return text === 'I agree to deleting my current private key' || 'Incorrect text'
     },
-    setKey(newKey) {
-      if (!this.privateKeyValidated) return
-      this.nextParams[0] = newKey
-      this.nextAction = 'setKey'
-      this.confirm = true
+    setKey() {
+      if (this.$refs.customPrivateKeyForm.validate()) {
+        this.nextParams[0] = this.customPrivateKey
+        this.nextAction = 'setKey'
+        this.confirm = true
+      }
     },
     resetKey() {
       this.nextAction = 'resetKey'
@@ -138,16 +158,18 @@ export default {
       this.confirm = false
     },
     agree() {
-      if (this.nextAction === 'setKey') {
-        this.$emit('setKey', stripHexPrefix(this.prevKey), this.nextParams[0])
-        this.$emit('dialogClose')
-      } else if (this.nextAction === 'resetKey') {
-        this.$emit('resetKey', stripHexPrefix(this.prevKey))
-        this.$emit('dialogClose')
-      } else {
-        log.error('invalid next action', this.nextAction)
+      if (this.$refs.warningForm.validate()) {
+        if (this.nextAction === 'setKey') {
+          this.$emit('setKey', stripHexPrefix(this.prevKey), this.nextParams[0])
+          this.$emit('dialogClose')
+        } else if (this.nextAction === 'resetKey') {
+          this.$emit('resetKey', stripHexPrefix(this.prevKey))
+          this.$emit('dialogClose')
+        } else {
+          log.error('invalid next action', this.nextAction)
+        }
+        this.clearConfirmVars()
       }
-      this.clearConfirmVars()
     },
     disagree() {
       this.clearConfirmVars()
