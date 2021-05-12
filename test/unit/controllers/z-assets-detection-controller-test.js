1985dff804fe1e26bf1dc8af57d503a70c61f64e
himanshuchawla009 <himanshuchawla2014@gmail.com>
2021-05-12 10:39:38 +0530

    detect collectibles from covalent test case added

--- test/unit/controllers/z-assets-detection-controller-test.js
+++ test/unit/controllers/z-assets-detection-controller-test.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import assert from 'assert'
 import nock from 'nock'
 import { createSandbox } from 'sinon'
@@ -58,16 +57,107 @@ describe('AssetsDetectionController', () => {
       assetController: assets,
       getCovalentNfts: prefsController.getCovalentNfts.bind(prefsController),
     })
-    console.log(COVALENT_API, 'api')
     nock(COVALENT_API)
-      .get('/covalent')
+      .get('/covalent?url=https://api.covalenthq.com/v1/1/address/0x0DCD5D886577d5081B0c52e242Ef29E70Be3E7bc/balances_v2/')
       .reply(200, {
         data: {
-          description: 'Description',
-          image_url: 'url',
-          name: 'Name',
-          symbol: 'FOO',
-          total_supply: 0,
+          data: {
+            address: '0x0DCD5D886577d5081B0c52e242Ef29E70Be3E7bc',
+            updated_at: '2021-05-11T18:42:05.805590708Z',
+            next_update_at: '2021-05-11T18:47:05.805591268Z',
+            quote_currency: 'USD',
+            chain_id: 1,
+            items: [
+              {
+                contract_decimals: 0,
+                contract_name: 'MARBLE-NFT',
+                contract_ticker_symbol: 'MRBLNFT',
+                contract_address: '0x1d963688FE2209A98db35c67A041524822CF04gg',
+                supports_erc: ['erc20', 'erc721'],
+                logo_url: 'url',
+                type: 'nft',
+                balance: '2',
+                quote_rate: 0,
+                quote: 0,
+                nft_data: [
+                  {
+                    token_id: '2577',
+                    token_balance: '1',
+                    token_url: 'https://ws.marble.cards/marble/token/11740',
+                    supports_erc: ['erc20', 'erc721'],
+                    token_price_wei: null,
+                    token_quote_rate_eth: null,
+                    external_data: {
+                      name: 'ID 2577',
+                      description: 'Description 2577',
+                      image: 'url GG',
+                      owner: '0x0DCD5D886577d5081B0c52e242Ef29E70Be3E7bc',
+                    },
+                    owner: '0x0DCD5D886577d5081B0c52e242Ef29E70Be3E7bc',
+                  },
+                ],
+              },
+              {
+                contract_decimals: 0,
+                contract_name: 'MARBLE-NFT',
+                contract_ticker_symbol: 'MRBLNFT',
+                contract_address: '0x1d963688FE2209A98db35c67A041524822CF04ii',
+                supports_erc: ['erc20', 'erc721'],
+                logo_url: 'url',
+                type: 'nft',
+                balance: '2',
+                quote_rate: 0,
+                quote: 0,
+                nft_data: [
+                  {
+                    token_id: '2578',
+                    token_balance: '1',
+                    token_url: 'https://ws.marble.cards/marble/token/11740',
+                    supports_erc: ['erc20', 'erc721'],
+                    token_price_wei: null,
+                    token_quote_rate_eth: null,
+                    external_data: {
+                      name: 'ID 2578',
+                      description: 'Description 2578',
+                      image: 'url II',
+                      owner: '0x0DCD5D886577d5081B0c52e242Ef29E70Be3E7bc',
+                    },
+                    owner: '0x0DCD5D886577d5081B0c52e242Ef29E70Be3E7bc',
+                  },
+                ],
+              },
+              {
+                contract_decimals: 0,
+                contract_name: 'MARBLE-NFT',
+                contract_ticker_symbol: 'MRBLNFT',
+                contract_address: '0x1d963688FE2209A98db35c67A041524822CF04hh',
+                supports_erc: ['erc20', 'erc721'],
+                logo_url: 'url',
+                type: 'nft',
+                balance: '2',
+                quote_rate: 0,
+                quote: 0,
+                nft_data: [
+                  {
+                    token_id: '2574',
+                    token_balance: '1',
+                    token_url: 'https://ws.marble.cards/marble/token/11740',
+                    supports_erc: ['erc20', 'erc721'],
+                    token_price_wei: null,
+                    token_quote_rate_eth: null,
+                    external_data: {
+                      name: 'ID 2574',
+                      description: 'Description 2574',
+                      image: 'url HH',
+                      external_url: 'https://marble.cards/card/11740',
+                      owner: '0x0DCD5D886577d5081B0c52e242Ef29E70Be3E7bc',
+                    },
+                    owner: '0x0DCD5D886577d5081B0c52e242Ef29E70Be3E7bc',
+                  },
+                ],
+              },
+            ],
+          },
         },
       })
       .persist(true)
@@ -299,34 +389,40 @@ describe('AssetsDetectionController', () => {
       resolve()
     }))
 
-  // it('should detect and add collectibles correctly', async () => {
-  //   network.setProviderType(MAINNET)
-  //   assetsDetection.selectedAddress = TEST_ADDRESS
-  //   await assetsDetection.detectCollectibles()
-  //   assert.deepStrictEqual(assets.state.collectibles, [
-  //     {
-  //       address: '0x1d963688FE2209A98db35c67A041524822CF04gg',
-  //       tokenId: '2577',
-  //       name: 'ID 2577',
-  //       image: 'url GG',
-  //       description: 'Description 2577',
-  //     },
-  //     {
-  //       address: '0x1d963688FE2209A98db35c67A041524822CF04ii',
-  //       tokenId: '2578',
-  //       name: 'ID 2578',
-  //       image: 'url II',
-  //       description: 'Description 2578',
-  //     },
-  //     {
-  //       address: '0x1d963688FE2209A98db35c67A041524822CF04hh',
-  //       tokenId: '2574',
-  //       name: 'ID 2574',
-  //       image: 'url HH',
-  //       description: 'Description 2574',
-  //     },
-  //   ])
-  // })
+  it('should detect and add collectibles correctly', async () => {
+    network.setProviderType(MAINNET)
+    assetsDetection.selectedAddress = TEST_ADDRESS
+    await assetsDetection.detectCollectibles()
+    assert.deepStrictEqual(assets.state.collectibles, [
+      {
+        address: '0x1d963688FE2209A98db35c67A041524822CF04gg',
+        tokenId: '2577',
+        name: 'ID 2577',
+        image: 'url GG',
+        standard: 'erc721',
+        tokenBalance: '1',
+        description: 'Description 2577',
+      },
+      {
+        address: '0x1d963688FE2209A98db35c67A041524822CF04ii',
+        tokenId: '2578',
+        name: 'ID 2578',
+        image: 'url II',
+        standard: 'erc721',
+        tokenBalance: '1',
+        description: 'Description 2578',
+      },
+      {
+        address: '0x1d963688FE2209A98db35c67A041524822CF04hh',
+        tokenId: '2574',
+        name: 'ID 2574',
+        image: 'url HH',
+        standard: 'erc721',
+        tokenBalance: '1',
+        description: 'Description 2574',
+      },
+    ])
+  })
 
   it('should not detect and add collectibles if there is no selectedAddress', async () => {
     await assetsDetection.detectCollectibles()
