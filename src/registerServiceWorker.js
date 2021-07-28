3d937df1d491129a4dd798497116eab319ce4806
chaitanyapotti <chaitanya.potti@gmail.com>
2021-07-28 10:14:14 +0800

    Fix build issue

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -145,107 +145,110 @@ if (navigator.userAgent.includes('Firefox')) {
     .getRegistrations()
     .then((x) => x.map((y) => y.unregister()))
     .catch((error) => log.error(error))
-  return
+} else {
+  swregister()
 }
 
-if (
-  'serviceWorker' in navigator &&
-  (process.env.VUE_APP_TORUS_BUILD_ENV === 'production' ||
-    process.env.VUE_APP_TORUS_BUILD_ENV === 'binance' ||
-    process.env.VUE_APP_TORUS_BUILD_ENV === 'testing' ||
-    process.env.VUE_APP_TORUS_BUILD_ENV === 'lrc')
-) {
-  log.info('non-integrity sw')
-  // if swIntegrity is not calculated
-  if (swIntegrity === ['SERVICE', 'WORKER', 'SHA', 'INTEGRITY'].join('_')) {
-    register({
-      onUpdate: (registration) => {
-        const waitingServiceWorker = registration.waiting
+function swregister() {
+  if (
+    'serviceWorker' in navigator &&
+    (process.env.VUE_APP_TORUS_BUILD_ENV === 'production' ||
+      process.env.VUE_APP_TORUS_BUILD_ENV === 'binance' ||
+      process.env.VUE_APP_TORUS_BUILD_ENV === 'testing' ||
+      process.env.VUE_APP_TORUS_BUILD_ENV === 'lrc')
+  ) {
+    log.info('non-integrity sw')
+    // if swIntegrity is not calculated
+    if (swIntegrity === ['SERVICE', 'WORKER', 'SHA', 'INTEGRITY'].join('_')) {
+      register({
+        onUpdate: (registration) => {
+          const waitingServiceWorker = registration.waiting
 
-        if (waitingServiceWorker) {
-          waitingServiceWorker.addEventListener('statechange', (event) => {
-            if (event?.target?.state === 'activated' && isMain) window.location.reload()
+          if (waitingServiceWorker) {
+            waitingServiceWorker.addEventListener('statechange', (event) => {
+              if (event?.target?.state === 'activated' && isMain) window.location.reload()
+            })
+            waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' })
+          }
+        },
+      })
+    } else {
+      log.info('full integrity sw')
+      // Check on existing service worker registration(s)
+      // if there are errors, remove all service workers first
+      let swRegistrations
+      navigator.serviceWorker
+        .getRegistration()
+        .then((reg) => {
+          log.info('checking existing service worker registration')
+          return new Promise((resolve) => {
+            const response = {
+              err: null,
+              sw: null,
+            }
+            if (reg === undefined) {
+              response.err = new Error('no service worker installed')
+            } else if (reg.updateViaCache !== 'all') {
+              response.err = new Error('updateViaCache should be "all"')
+            } else if (new URL(reg.active.scriptURL).pathname !== serviceWorkerUrl) {
+              response.err = new Error(`unexpected scriptURL ${new URL(reg.active.scriptURL).pathname}, expected ${serviceWorkerUrl}`)
+            } else {
+              response.sw = reg
+            }
+            resolve(response)
           })
-          waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' })
-        }
-      },
-    })
-  } else {
-    log.info('full integrity sw')
-    // Check on existing service worker registration(s)
-    // if there are errors, remove all service workers first
-    let swRegistrations
-    navigator.serviceWorker
-      .getRegistration()
-      .then((reg) => {
-        log.info('checking existing service worker registration')
-        return new Promise((resolve) => {
-          const response = {
-            err: null,
-            sw: null,
-          }
-          if (reg === undefined) {
-            response.err = new Error('no service worker installed')
-          } else if (reg.updateViaCache !== 'all') {
-            response.err = new Error('updateViaCache should be "all"')
-          } else if (new URL(reg.active.scriptURL).pathname !== serviceWorkerUrl) {
-            response.err = new Error(`unexpected scriptURL ${new URL(reg.active.scriptURL).pathname}, expected ${serviceWorkerUrl}`)
-          } else {
-            response.sw = reg
-          }
-          resolve(response)
         })
-      })
-      .then((responseObject) => {
-        // if there were errors, we need to re-register the service worker
-        if (responseObject.err) {
-          const finalArray = []
-          finalArray.push(navigator.serviceWorker.register(serviceWorkerUrl, { updateViaCache: 'all', scope: process.env.BASE_URL }))
-          return Promise.all(finalArray)
-        }
-        return Promise.all([Promise.resolve(responseObject.sw)])
-      })
-      .then((swRegs) => {
-        log.info(swRegs, 'final regs')
-        swRegistrations = swRegs
-        // Although the service worker is registered, its integrity has not been checked.
-        // This is impossible to circumvent, service worker initial registrations always
-        // bypass HTML cache. Instead, we ensure that the service worker was already registered,
-        // force-fetch the service worker file from the server and check that its cached
-        // and then we use this cached file to *update* the service worker
-        return fetch(serviceWorkerUrl, {
-          cache: 'reload',
+        .then((responseObject) => {
+          // if there were errors, we need to re-register the service worker
+          if (responseObject.err) {
+            const finalArray = []
+            finalArray.push(navigator.serviceWorker.register(serviceWorkerUrl, { updateViaCache: 'all', scope: process.env.BASE_URL }))
+            return Promise.all(finalArray)
+          }
+          return Promise.all([Promise.resolve(responseObject.sw)])
         })
-      })
-      .then(async (resp) => {
-        // if Cache-Control headers are not as expected, throw
-        if (resp.headers.get('Cache-Control') !== expectedCacheControlHeader) {
-          throw new Error(`Unexpected Cache-Control headers, got ${resp.headers.get('Cache-Control')}`)
-        }
-        // if response data fails integrity check, throw
-        const text = await resp.text()
-        const integrity = generateIntegrity(
-          {
-            algorithms: ['sha384'],
-          },
-          text
-        )
-        if (integrity !== swIntegrity) {
-          throw new Error(`Service worker integrity check failed, expected ${swIntegrity} got ${integrity}`)
-        }
-        // update the service worker, which should fetch the file from cache
-        return swRegistrations && swRegistrations.forEach((x) => x.update())
-      })
-      .catch((error) => {
-        // if failed to fetch, throw
-        throw new Error(`Could not fetch service worker from server, ${error.toString()}`)
-      })
-      .then((updatedSwRegs) => {
-        log.info('Successfully registered secure service worker', updatedSwRegs)
-      })
-      .catch((error) => {
-        log.warn('Could not complete service worker installation process, error: ', error)
-        // throw new Error('Could not install service worker')
-      })
+        .then((swRegs) => {
+          log.info(swRegs, 'final regs')
+          swRegistrations = swRegs
+          // Although the service worker is registered, its integrity has not been checked.
+          // This is impossible to circumvent, service worker initial registrations always
+          // bypass HTML cache. Instead, we ensure that the service worker was already registered,
+          // force-fetch the service worker file from the server and check that its cached
+          // and then we use this cached file to *update* the service worker
+          return fetch(serviceWorkerUrl, {
+            cache: 'reload',
+          })
+        })
+        .then(async (resp) => {
+          // if Cache-Control headers are not as expected, throw
+          if (resp.headers.get('Cache-Control') !== expectedCacheControlHeader) {
+            throw new Error(`Unexpected Cache-Control headers, got ${resp.headers.get('Cache-Control')}`)
+          }
+          // if response data fails integrity check, throw
+          const text = await resp.text()
+          const integrity = generateIntegrity(
+            {
+              algorithms: ['sha384'],
+            },
+            text
+          )
+          if (integrity !== swIntegrity) {
+            throw new Error(`Service worker integrity check failed, expected ${swIntegrity} got ${integrity}`)
+          }
+          // update the service worker, which should fetch the file from cache
+          return swRegistrations && swRegistrations.forEach((x) => x.update())
+        })
+        .catch((error) => {
+          // if failed to fetch, throw
+          throw new Error(`Could not fetch service worker from server, ${error.toString()}`)
+        })
+        .then((updatedSwRegs) => {
+          log.info('Successfully registered secure service worker', updatedSwRegs)
+        })
+        .catch((error) => {
+          log.warn('Could not complete service worker installation process, error: ', error)
+          // throw new Error('Could not install service worker')
+        })
+    }
   }
 }
