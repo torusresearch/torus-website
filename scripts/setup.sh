f8b070603944d5420809874170faa0f39efccd7f
chaitanyapotti <chaitanya.potti@gmail.com>
2021-11-15 15:14:30 +0800

    setup ssh keys first in ci

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,8 @@
 git config --global user.email "no-reply@tor.us"
 git config --global user.name "torus-bot"
+mkdir -p ~/.ssh 
+echo "$GITHUB_SSH_KEY" >> ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 git add package-lock.json
 git commit -m "update package-lock.json"
