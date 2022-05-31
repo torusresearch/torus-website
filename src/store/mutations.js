0b3bc722585c3cf8eaaace527f7401603031f8dd
arch1995 <arch1995@gmail.com>
2022-05-31 14:01:57 +0530

    Prevent sw reload

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -234,6 +234,9 @@ export default {
     state.isTkeySeedPhraseInputRequired = payload
   },
   setLoginInProgress(state, payload) {
+    if (typeof window != 'undefined') {
+      window.loginInProcess = payload
+    }
     state.embedState = { ...state.embedState, loginInProgress: payload }
   },
   setAnnouncements(state, payload) {
