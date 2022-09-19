29daf9eac5f8f5f7163ee2d0f19cb4c193ddd869
Pradeep <pradeep@tor.us>
2022-09-19 18:36:07 +0530

    minor: login title change for dapp

--- src/components/Login/LoginTitle/LoginTitle.vue
+++ src/components/Login/LoginTitle/LoginTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-flex class="mb-2 mb-sm-0" :class="isDapp ? '' : 'ml-auto mr-auto xs10 sm8'">
+  <v-flex v-if="!isDapp" class="mb-2 mb-sm-0" :class="isDapp ? '' : 'ml-auto mr-auto xs10 sm8'">
     <div
       class="text_2--text"
       :class="isDapp ? 'headline font-weight-medium' : 'display-1 font-weight-bold'"
@@ -32,6 +32,15 @@
       {{ t('login.titleNew') }}
     </div>
   </v-flex>
+  <flex v-else class="mb-2 mb-sm-0">
+    <div
+      class="text_2--text"
+      :class="isDapp ? 'headline font-weight-medium' : 'display-1 font-weight-bold'"
+      :style="{ maxWidth: isDapp ? 'inherit' : '260px' }"
+    >
+      {{ t('login.title') }}
+    </div>
+  </flex>
 </template>
 
 <script>
