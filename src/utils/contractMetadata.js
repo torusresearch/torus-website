6268d0f63284d9762ae833791327151962219000
himanshuchawla009 <himanshuchawla2014@gmail.com>
2022-10-03 11:32:16 +0800

    add gnosis erc20 token by default

--- src/utils/contractMetadata.js
+++ src/utils/contractMetadata.js
@@ -10,3 +10,13 @@ export default {
     decimals: 18,
   },
 }
+
+export const GNOSIS_CONTRACTS = {
+  '0x6810e776880c02933d47db1b9fc05908e5386b96': {
+    name: 'Gnosis',
+    logo: 'https://docs.gnosischain.com/img/tokens/gno.png',
+    erc20: true,
+    symbol: 'GNO',
+    decimals: 18,
+  },
+}
