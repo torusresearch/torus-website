5bde2669d93386371860a812a42bc0534b0e04b0
chaitanyapotti <chaitanya.potti@gmail.com>
2022-11-11 09:52:08 +0100

    Fix UI & improve logic

--- src/containers/WalletSwap/WalletSwap.vue
+++ src/containers/WalletSwap/WalletSwap.vue
@@ -1,19 +1,70 @@
 <template>
   <v-container class="dapp-parent d-flex flex-column justify-start align-center" :class="$vuetify.breakpoint.xsOnly ? 'xs-parent px-4' : ''">
-    <v-form v-model="valid">
-      <div class="body-2 mb-2">From Token</div>
-      <span>
-        <v-text-field :value="fromValue" required type="number" @change="fromValueChanged" />
-        <v-combobox :value="fromToken" :items="tokenSymbols" required @change="fromTokenChanged" />
-      </span>
-      <div class="body-2 mb-2">To Token</div>
-      <span>
-        <v-text-field :value="toValue" type="number" @change="toValueChanged" />
-        <v-combobox :value="toToken" :items="tokenSymbols" required @input="toTokenChanged" />
-      </span>
-      <div>
-        <v-btn @click="sendTx">Submit</v-btn>
-      </div>
+    <v-form ref="form" v-model="valid" lazy-validation @submit.prevent="sendTx">
+      <v-card class="swap-container elevation-1 pa-4">
+        <div class="font-weight-bold mb-2">Swap</div>
+        <v-card flat outlined class="mb-2 pa-1">
+          <v-row class="align-center" no-gutters>
+            <v-col>
+              <v-text-field
+                class="swap-amount"
+                :value="fromValue"
+                hide-details
+                outlined
+                type="number"
+                :rules="[rules.required]"
+                @change="fromValueChanged"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="3">
+              <v-combobox
+                :value="fromToken"
+                class="swap-token"
+                flat
+                outlined
+                hide-details
+                solo
+                :items="tokenList"
+                item-text="symbol"
+                item-value="symbol"
+                :rules="[rules.required]"
+                @change="fromTokenChanged"
+              ></v-combobox>
+            </v-col>
+          </v-row>
+        </v-card>
+        <v-card flat outlined class="pa-1 mb-4">
+          <v-row class="align-center" no-gutters>
+            <v-col>
+              <v-text-field class="swap-amount" :value="toValue" readonly hide-details outlined type="number" @change="toValueChanged"></v-text-field>
+            </v-col>
+            <v-col cols="3">
+              <v-combobox
+                :value="toToken"
+                class="swap-token"
+                flat
+                outlined
+                hide-details
+                solo
+                :items="tokenList"
+                item-text="symbol"
+                item-value="symbol"
+                :rules="[rules.required]"
+                @change="toTokenChanged"
+              ></v-combobox>
+            </v-col>
+          </v-row>
+        </v-card>
+        <div v-if="!!currentSwapQuote">
+          <div>Expected Output: {{ toValue }} {{ toToken }}</div>
+          <div>Price Impact: {{ priceImpact }} %</div>
+          <div>Network Fee: {{ gasFees }} USD</div>
+        </div>
+        <div v-else-if="!currentSwapQuote && valid">
+          <div>Fetching best price</div>
+        </div>
+        <v-btn class="text-h6 mt-2" color="primary" x-large block type="submit" :loading="sendingTx">Swap</v-btn>
+      </v-card>
     </v-form>
   </v-container>
 </template>
@@ -37,11 +88,16 @@ export default {
     return {
       fromValue: 0.01,
       toValue: 0,
-      fromToken: 'MATIC',
-      toToken: 'USDC',
-      valueRules: [(v) => !!v || 'Value is required'],
-      swapQuote: null,
-      valid: true,
+      fromToken: '',
+      toToken: '',
+      rules: {
+        required: (v) => !!v || 'Value is required',
+      },
+      currentSwapQuote: null,
+      valid: false,
+      priceImpact: 0,
+      gasFees: 0,
+      sendingTx: false,
     }
   },
   computed: {
@@ -72,17 +128,18 @@ export default {
     },
   },
   async mounted() {
+    this.fromToken = this.nativeToken.symbol
     this.getSwapQuote()
   },
   methods: {
     fromTokenChanged(value) {
       log.info('fromTokenChanged', value)
-      this.fromToken = value
+      this.fromToken = value.symbol
       this.getSwapQuote()
     },
     toTokenChanged(value) {
       log.info('toTokenChanged', value)
-      this.toToken = value
+      this.toToken = value.symbol
       this.getSwapQuote()
     },
     fromValueChanged(value) {
@@ -94,10 +151,10 @@ export default {
       this.getSwapQuote()
     },
     async getSwapQuote() {
-      log.info('getSwapQuote')
-      if (!this.fromToken || !this.toToken || !this.fromValue) {
-        return
-      }
+      const valid = this.$refs.form.validate()
+      log.info(valid, 'valid')
+      log.info('getting swap quote after validation', valid)
+      if (!valid) return
 
       log.info(this.chainId, 'chainId')
       const fromTokenInstance = this.getTokenInstance(this.fromToken)
@@ -118,19 +175,19 @@ export default {
           slippageTolerance: new Percent(5, 100),
           deadline: Math.floor(Date.now() / 1000 + 1800),
           fee: {
-            fee: new Percent(50, 100), // use 50% fees
+            fee: new Percent(1, 100), // use 1% fees
             recipient: '0x3E2a1F4f6b6b5d281Ee9a9B36Bb33F7FBf0614C3',
           },
         },
-        {
-          maxSwapsPerPath: 10,
-        }
+        {}
       )
       log.info(swapRoute)
-      log.info(`Quote Exact In: ${swapRoute.quote.toFixed(2)}`)
-      log.info(`Gas Adjusted Quote In: ${swapRoute.quoteGasAdjusted.toFixed(2)}`)
-      log.info(`Gas Used USD: ${swapRoute.estimatedGasUsedUSD.toFixed(6)}`)
-      this.toValue = swapRoute.quote.toFixed(2)
+      log.info(`Quote Exact In: ${swapRoute.quote.toSignificant()}`)
+      log.info(`Gas Adjusted Quote In: ${swapRoute.quoteGasAdjusted.toSignificant()}`)
+      log.info(`Gas Used USD: ${swapRoute.estimatedGasUsedUSD.toSignificant()}`)
+      this.toValue = swapRoute.quote.toSignificant()
+      this.priceImpact = swapRoute.trade.priceImpact.toFixed(2)
+      this.gasFees = swapRoute.estimatedGasUsedUSD.toSignificant()
       this.currentSwapQuote = swapRoute
     },
     getTokenInstance(symbol) {
@@ -148,6 +205,7 @@ export default {
       if (!this.currentSwapQuote) {
         return
       }
+      this.sendingTx = true
       const transaction = {
         data: this.currentSwapQuote.methodParameters.calldata,
         to: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45',
@@ -156,7 +214,13 @@ export default {
         gasPrice: this.currentSwapQuote.gasPriceWei.toHexString(),
       }
       log.info(transaction)
-      await torus.web3.eth.sendTransaction(transaction)
+      torus.web3.eth.sendTransaction(transaction, (err, txHash) => {
+        log.info(err, txHash)
+        this.sendingTx = false
+        // TODO: Handle error and success
+        // Maybe show tx success modal in etherscan
+        // Do correct etherscan url depending on chainId from wallet transfer page
+      })
     },
   },
 }
