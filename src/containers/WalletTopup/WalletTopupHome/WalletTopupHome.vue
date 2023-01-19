3ce676e9c498dddd4cbb58a270b2d142e48b0723
Naveed Elahi <statix231@hotmail.com>
2023-01-19 18:22:55 +0800

    remove change network btn on top up header
    its redundant due to being able to switch network using the pill

--- src/containers/WalletTopup/WalletTopupHome/WalletTopupHome.vue
+++ src/containers/WalletTopup/WalletTopupHome/WalletTopupHome.vue
@@ -9,12 +9,6 @@
         <v-icon size="22" class="black--text mr-2">$vuetify.icons.alert</v-icon>
         <div class="text-caption text-sm-body-2">{{ t('walletTopUp.topUpNotSupported', [networkType.networkName]) }}</div>
       </div>
-
-      <div class="text-right" style="margin-top: -4px">
-        <router-link class="text-uppercase text-decoration-none text-body-2 ml-1" :to="{ name: 'walletSettings' }" :style="{ lineHeight: 0 }">
-          {{ t('walletTopUp.changeNetwork') }}
-        </router-link>
-      </div>
     </div>
 
     <NoSupportedProvidersForNetworkPlaceholder v-if="noSupportedProvidersForNetwork">
