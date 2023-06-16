7bea9ea77b9c3f2dbb8898f117abdf9469a1abcb
Lionell Briones <llenoil@gmail.com>
2023-06-16 17:30:10 +0800

    remove paybis widget

--- src/store/PaymentActions/paybis.js
+++ src/store/PaymentActions/paybis.js
@@ -1,6 +1,9 @@
+import config from '../../config'
+import PopupHandler from '../../handlers/Popup/PopupHandler'
+import PopupWithBcHandler from '../../handlers/Popup/PopupWithBcHandler'
 import { getQuote, getWalletOrder, saveWalletOrder } from '../../plugins/paybis'
 import { ETH } from '../../utils/enums'
-import { paymentProviders } from '../../utils/utils'
+import { paymentProviders, randomId } from '../../utils/utils'
 
 export default {
   fetchPaybisQuote({ state }, payload) {
@@ -14,7 +17,7 @@ export default {
       { Authorization: `Bearer ${state.jwtToken[state.selectedAddress]}` }
     )
   },
-  fetchPaybisOrder({ dispatch, state }, { currentOrder, selectedAddress }) {
+  fetchPaybisOrder({ dispatch, state }, { currentOrder, preopenInstanceId: preopenInstanceIdPayload, selectedAddress }) {
     return new Promise((resolve, reject) => {
       const parameters = {
         target: currentOrder.target,
@@ -26,38 +29,58 @@ export default {
         .then(({ data }) => {
           // https://widget.sandbox.paybis.com/?requestId=a2d1153b-4588-44ae-882d-8120b59fe13a#/v2/exchange-form
 
-          window.PartnerExchangeWidget.open({
-            requestId: data.requestId,
-          })
-
-          window.PartnerExchangeWidget.events.onclosed = () => {
-            // eslint-disable-next-line no-console
-            console.log('PartnerExchangeWidget.state', window.PartnerExchangeWidget.state)
-            if (window.PartnerExchangeWidget.state === 'completed') {
-              // Save to backend
-              const orderParams = {
-                paybis_id: data.requestId,
-                wallet_address: selectedAddress,
-                fiat_currency: currentOrder.from,
-                crypto_currency: currentOrder.target,
-                fiat_amount: currentOrder.fiatAmount,
-                crypto_amount: currentOrder.targetAmount,
-              }
-
-              dispatch('postPaybisOrder', { orderParams })
-
-              resolve({ success: true })
-            } else {
-              reject(new Error('user closed paybis popup'))
-            }
+          let preopenInstanceId = preopenInstanceIdPayload
+          if (!preopenInstanceId) {
+            preopenInstanceId = randomId()
+            const finalUrl = `${config.redirect_uri}?preopenInstanceId=${preopenInstanceId}`
+            const handledWindow = new PopupHandler({ url: finalUrl })
+            handledWindow.open()
+            handledWindow.once('close', () => {
+              reject(new Error('user closed Paybis popup'))
+            })
           }
+
+          const orderInstanceId = randomId()
+          const url = `https://widget.sandbox.paybis.com/?requestId=${data.requestId}#/v2/exchange-form`
+
+          dispatch('postPaybisOrder', { finalUrl: url, preopenInstanceId, orderInstanceId }).then(resolve).catch(reject)
+
+          // window.PartnerExchangeWidget.open({
+          //   requestId: data.requestId,
+          // })
+          // window.PartnerExchangeWidget.events.onclosed = () => {
+          //   // eslint-disable-next-line no-console
+          //   console.log('PartnerExchangeWidget.state', window.PartnerExchangeWidget.state)
+          //   if (window.PartnerExchangeWidget.state === 'completed') {
+          //     // Save to backend
+          //     const orderParams = {
+          //       paybis_id: data.requestId,
+          //       wallet_address: selectedAddress,
+          //       fiat_currency: currentOrder.from,
+          //       crypto_currency: currentOrder.target,
+          //       fiat_amount: currentOrder.fiatAmount,
+          //       crypto_amount: currentOrder.targetAmount,
+          //     }
+          //     dispatch('postPaybisOrder', { orderParams })
+          //     resolve({ success: true })
+          //   } else {
+          //     reject(new Error('user closed paybis popup'))
+          //   }
+          // }
         })
         .catch((error) => {
           reject(error)
         })
     })
   },
-  postPaybisOrder({ state }, { orderParams }) {
+  async postPaybisOrder(_, { finalUrl, preopenInstanceId, orderInstanceId }) {
+    const paybisWindow = new PopupWithBcHandler({ url: finalUrl, preopenInstanceId, channelName: `redirect_channel_${orderInstanceId}` })
+    const result = await paybisWindow.handle()
+    // const { queryParams: { transactionStatus = '' } = {} } = result
+    // eslint-disable-next-line no-console
+    console.log('result', result)
+  },
+  savePaybisOrder({ state }, { orderParams }) {
     saveWalletOrder(orderParams, { Authorization: `Bearer ${state.jwtToken[state.selectedAddress]}` })
   },
 }
