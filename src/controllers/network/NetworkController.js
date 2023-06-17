058880185ad22a6b5edd0fb73362da3811dcc125
chaitanyapotti <chaitanya.potti@gmail.com>
2023-06-17 22:55:07 +0800

    fix chain switch with request

--- src/controllers/network/NetworkController.js
+++ src/controllers/network/NetworkController.js
@@ -575,6 +575,20 @@ export default class NetworkController extends EventEmitter {
     engine.push(metamaskMiddleware)
     engine.push(networkMiddleware)
     const provider = providerFromEngine(engine)
+    provider.request = async (args) => {
+      const req = {
+        ...args,
+        id: Math.random().toString(36).slice(2),
+        jsonrpc: '2.0',
+      }
+      return new Promise((resolve, reject) => {
+        provider.sendAsync(req, (err, res) => {
+          if (err) reject(err)
+          else if (res.error) reject(res.error)
+          else resolve(res.result)
+        })
+      })
+    }
     this._setProviderAndBlockTracker({ provider, blockTracker })
   }
 
--- src/controllers/network/createInfuraClient.js
+++ src/controllers/network/createInfuraClient.js
@@ -1,12 +1,12 @@
-import { createInfuraMiddleware, createProvider } from '@metamask/eth-json-rpc-infura'
+import { createInfuraMiddleware } from '@metamask/eth-json-rpc-infura'
 import {
   createBlockCacheMiddleware,
   createBlockRefMiddleware,
   createBlockTrackerInspectorMiddleware,
   createInflightCacheMiddleware,
   createRetryOnEmptyMiddleware,
-  // providerFromMiddleware,
 } from '@metamask/eth-json-rpc-middleware'
+import { providerFromMiddleware } from '@metamask/eth-json-rpc-provider'
 import { createScaffoldMiddleware, mergeMiddleware } from '@toruslabs/openlogin-jrpc'
 import { PollingBlockTracker } from 'eth-block-tracker'
 
@@ -15,7 +15,7 @@ import { INFURA_NETWORK_TYPE_TO_ID_MAP } from '../../utils/enums'
 
 export function createInfuraClient({ network }) {
   const infuraMiddleware = createInfuraMiddleware({ network, projectId: config.infuraKey })
-  const infuraProvider = createProvider({ network, projectId: config.infuraKey })
+  const infuraProvider = providerFromMiddleware(infuraMiddleware)
   const blockTracker = new PollingBlockTracker({ provider: infuraProvider })
   const networkMiddleware = mergeMiddleware([
     createNetworkAndChainIdMiddleware({ network }),
