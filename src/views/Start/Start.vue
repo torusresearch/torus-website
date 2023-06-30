224e55f51ef6bd9b4f338715f509dc6b69e5c62f
Lionell Briones <llenoil@gmail.com>
2023-06-30 12:26:27 +0800

    use logo loader on openlogin

--- src/views/Start/Start.vue
+++ src/views/Start/Start.vue
@@ -59,6 +59,11 @@ export default {
         whiteLabel.theme.colors = { torusBrand1: this.$vuetify.theme.currentTheme.torusBrand1 }
         whiteLabel.defaultLanguage = this.$i18n.locale
       }
+
+      whiteLabel.useLogoLoader = true
+      whiteLabel.logoDark = 'https://images.web3auth.io/wallet-exchanga.svg'
+      whiteLabel.logoLight = 'https://images.web3auth.io/wallet-exchanga-light.svg'
+
       this.whiteLabel = whiteLabel
 
       const openLoginHandler = await OpenLoginHandler.getInstance(whiteLabel, loginConfig, sessionNamespace)
