4a0dda5124624a896c30804396e7e535a1f744ef
Lionell Briones <llenoil@gmail.com>
2023-07-03 18:40:17 +0800

    update paybis url

--- src/config.js
+++ src/config.js
@@ -173,7 +173,7 @@ export default {
   mercuryoApiHost: 'https://mercuryo-api.tor.us',
   transakApiHost: 'https://transak-api.tor.us',
   banxaApiHost: 'https://banxa-api.tor.us',
-  paybisApiHost: 'http://localhost:2110',
+  paybisApiHost: 'https://paybis-api.tor.us',
 
   moonpayHost: 'https://buy.moonpay.io',
   moonpayApiQuoteHost: 'https://api.moonpay.io',
